66bda705be7a41c21c84cf58d3c939a00104845f
James Seward <james@jamesoff.net>
2003-01-02 23:29:07 +0000

    Added to allow loading of <langage>/nick_*.tcl plugins

---
+++ plugins/nick.tcl
@@ -0,0 +1,25 @@
+## bMotion plugins loader: nick
+#
+# $Id$
+#
+
+###############################################################################
+# This is a bMotion plugin
+# Copyright (C) James Michael Seward 2000-2002
+#
+# This program is covered by the GPL, please refer the to LICENCE file in the
+# distribution; further information can be found in the headers of the scripts
+# in the modules directory.
+###############################################################################
+
+set languages [split $bMotionSettings(languages) ","]
+foreach language $languages {
+  bMotion_putloglev 2 * "bMotion: loading nick plugins language = $language"
+  set files [glob -nocomplain "$bMotionPlugins/$language/nick_*.tcl"]
+  foreach f $files {
+    bMotion_putloglev 1 * "bMotion: loading ($language) nick plugin file $f"
+    catch {
+      source $f
+    }
+  }
+}
