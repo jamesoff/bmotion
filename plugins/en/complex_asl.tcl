2672b1555450e26544589ed285fd9d820e9489e9
James Seward <james@jamesoff.net>
2003-01-12 14:43:53 +0000

    moved from legacy code

---
+++ plugins/en/complex_asl.tcl
@@ -0,0 +1,23 @@
+# $Id$
+#
+
+###############################################################################
+# This is a bMotion plugin
+# Copyright (C) James Seward 2000-2003
+#
+# This program is covered by the GPL, please refer the to LICENCE file in the
+# distribution; further information can be found in the headers of the scripts
+# in the modules directory.
+###############################################################################
+
+bMotion_plugin_add_complex "asl" {[[:<:]]a/?s/?l\??[[:>:]]} 100 bMotion_plugin_complex_asl "en"
+  
+proc bMotion_plugin_complex_asl { nick host handle channel text } {
+  if {[bMotionTalkingToMe $text] || [rand 2]} {
+    set age [expr [rand 20] + 13]
+    global bMotionInfo
+    bMotionDoAction $channel $nick "%%: $age/$bMotionInfo(gender)/%VAR{locations}"
+    return 1
+  }
+  return 0
+}
