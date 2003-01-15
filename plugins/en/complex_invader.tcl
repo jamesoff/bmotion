7870c6578ef2d5dc04d553bf9ec9a12765cb89bf
James Seward <james@jamesoff.net>
2003-01-15 15:42:08 +0000

    Reduced chance of plugins firing because they're still too frequent The nick change plugin now also checks the same settings as the away nick handler to stop more flooding

--- plugins/en/complex_invader.tcl
+++ plugins/en/complex_invader.tcl
@@ -54,6 +54,24 @@ proc bMotion_plugin_complex_invader_nick { nick host handle channel newnick } {
 	#set nickresponse [ pickRandom $randomZimNameChange ]
 	#return $nickresponse 
   if {![bMotion_interbot_me_next $channel]} { return 0 }
+  
+  #check we haven't already done something for this nick
+  if {$nick == [bMotion_plugins_settings_get "complex:returned" "lastnick" $channel ""]} {
+    return 1
+  }
+
+  #check we haven't already done something for this nick
+  if {$nick == [bMotion_plugins_settings_get "complex:away" "lastnick" $channel ""]} {
+    return 1
+  }
+
+  #save as newnick because if they do a /me next it'll be their new nick
+  bMotion_plugins_settings_set "complex:away" "lastnick" $channel "" $newnick
+
+
+  #save as newnick because if they do a /me next it'll be their new nick
+  bMotion_plugins_settings_set "complex:returned" "lastnick" $channel "" $newnick
+  
   bMotionDoAction $channel $nick "%VAR{randomZimNameChange}"
   return 1
 }
@@ -249,14 +267,14 @@ set randomZimNameChange {
 # callbacks
 
 # "duty" plugin responds to "duty" and variations of "dootie"
-bMotion_plugin_add_complex "invader(duty)" "duty|doo+(t|d)(ie|y)" 40 "bMotion_plugin_complex_invader_duty" "en"
+bMotion_plugin_add_complex "invader(duty)" "duty|doo+(t|d)(ie|y)" 20 "bMotion_plugin_complex_invader_duty" "en"
 
 # "zim" plugin responds to "invade or invasion" "zim" "mwahahaha or hahaha" "victory for" "how dare" "you dare"
-bMotion_plugin_add_complex "invader(zim)" "zim|inva(de|sion)|((mwa)?ha(ha)+)|(victory for)|((you|how) dare)" 40 "bMotion_plugin_complex_invader_zim" "en"
+bMotion_plugin_add_complex "invader(zim)" "zim|inva(de|sion)|((mwa)?ha(ha)+)|(victory for)|((you|how) dare)" 20 "bMotion_plugin_complex_invader_zim" "en"
 
 # "gir" plugin responds to "gir" "whooo or wooo" "chicken" "doom" "piggy"
-bMotion_plugin_add_complex "invader(gir)" "w(h)?oo+|chicken|gir(!+| )|doo+m|piggy" 40 "bMotion_plugin_complex_invader_gir" "en"
+bMotion_plugin_add_complex "invader(gir)" "w(h)?oo+|chicken|gir(!+| )|doo+m|piggy" 20 "bMotion_plugin_complex_invader_gir" "en"
 
 # nick change response
-bMotion_plugin_add_nick_action "invader(nick)" ".*" 40 "bMotion_plugin_complex_invader_nick" "en"
+bMotion_plugin_add_nick_action "invader(nick)" ".*" 20 "bMotion_plugin_complex_invader_nick" "en"
 
