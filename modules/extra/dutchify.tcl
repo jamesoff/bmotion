a313c5ee3d46e2c81c75dcacba1454b374a30987
sabre2th <no@email.address>
2003-01-25 15:30:52 +0000

    - fixed a lot of errors, added some (very) basic grammar checking.   next item: smart verb translation.

--- modules/extra/dutchify.tcl
+++ modules/extra/dutchify.tcl
@@ -10,58 +10,55 @@
 # in the modules directory.
 ###############################################################################
 
+####################################
+# How this module works:
+# - first the input line is debrittified, removing al abbreviations etc.
+# - then common expressions are translated
+# - then single words are translated, even the substrings
+####################################
+
 proc bMotion_module_extra_dutchify_single_word_replace { in out line } {
   regsub -nocase -all "\[\[:<:\]\]$in\[\[:>:\]\]" $line $out line
   return $line
 }
 
+proc bMotion_module_extra_dutchify_single_word_replace_list { {inout} line } {
+  foreach {in out} $inout {
+	set line [bMotion_module_extra_dutchify_single_word_replace $in $out $line]
+  }
+  return $line
+}
+
+proc bMotion_module_extra_dutchify_makeDutch_multiWord { line } {
+  # this is mainly to replace multiple words before grammar is used
+  # also, this function isn't called from grammar, so it's less expensive
+  set line [string map -nocase {"french kiss" "tongzoen"} $line]
+
+  set line [bMotion_module_extra_dutchify_single_word_replace_list {
+	"(is|am) called" "heet"  "reserved sign" "gereserveerd bordje"  "to be" zijn
+	{a(n)?} {een}  "you are" "jij bent"  "are you" "ben je"
+  } $line]	
+
+  return $line
+}
+
 proc bMotion_module_extra_dutchify_makeDutch { line } {
+  # these are mainly one-word replacements
   set line [string map -nocase { cow koe } $line]
 
-  #i -> ik
-  set line [bMotion_module_extra_dutchify_single_word_replace i ik $line]
-  set line [bMotion_module_extra_dutchify_single_word_replace "(is|am) called" "heet" $line]
-  regsub -nocase -all {[[:<:]]a(n)?[[:>:]]} $line {een} line
-  set line [bMotion_module_extra_dutchify_single_word_replace it het $line]
-  set line [bMotion_module_extra_dutchify_single_word_replace am ben $line]
-  set line [bMotion_module_extra_dutchify_single_word_replace are zijn $line]
-  set line [bMotion_module_extra_dutchify_single_word_replace "reserved sign" "gereserveerd bordje" $line]
-  set line [bMotion_module_extra_dutchify_single_word_replace "to be" zijn $line]
-  set line [bMotion_module_extra_dutchify_single_word_replace no nee $line]
-  set line [bMotion_module_extra_dutchify_single_word_replace {is (.+)} {\1 is} $line]
-  set line [bMotion_module_extra_dutchify_single_word_replace he hij $line]
-  set line [bMotion_module_extra_dutchify_single_word_replace of van $line]
-  set line [bMotion_module_extra_dutchify_single_word_replace the de $line]
-  set line [bMotion_module_extra_dutchify_single_word_replace this dit $line]
-  set line [bMotion_module_extra_dutchify_single_word_replace at in $line]
-  set line [bMotion_module_extra_dutchify_single_word_replace on op $line]
-  set line [bMotion_module_extra_dutchify_single_word_replace and en $line]
-  set line [bMotion_module_extra_dutchify_single_word_replace has heeft $line]
-  set line [bMotion_module_extra_dutchify_single_word_replace yes ja $line]
-  set line [bMotion_module_extra_dutchify_single_word_replace how hoe $line]
-  set line [bMotion_module_extra_dutchify_single_word_replace who wie $line]
-  set line [bMotion_module_extra_dutchify_single_word_replace they zij $line]
-  set line [bMotion_module_extra_dutchify_single_word_replace do doe $line]
+  #regsub -nocase -all {[[:<:]]a(n)?[[:>:]]} $line {een} line
 
+  # then the more simple ones
+  set line [bMotion_module_extra_dutchify_single_word_replace_list {
+	it het  am ben  are zijn  i ik no nee  he hij  of van  or of
+	the de  at in  on op  and en  has heeft  yes ja  how hoe  who wie  they zij
+    do doe  air lucht  fuck neuk  miss mis  kiss kus  she zij  he hij
+  } $line]
 
-  #you -> je
-  regsub -nocase -all {[[:<:]]you[[:>:]]} $line {je} line
-
-  #...ed --> ge...d
-  regsub -nocase -all {(\w+)ed[[:>:]]} $line {ge\1d} line
-
-  #f... -> oph...
-  #regsub -nocase -all {[[:<:]]c(\w+)} $line {k\1} line
-
-  #...ify -> ver...en
-  regsub -nocase -all {[[:<:]](.+?)ify[[:>:]]} $line {ver\1en} line
-
-
-  set line [string map -nocase { "to be or not too be that is the question" "te zijn of niet te zijn, dat is de vraag" question vraag chicken kip hello hoi line lijn my mijn have heb nice leuke name naam stolen gestolen want wil must moet hugs knuffelt hug knuffel yes ja thanks dank} $line ]
+  set line [string map -nocase { question vraag chicken kip hello hoi line lijn my mijn have heb nice leuke name naam stolen gestolen want wil must moet hugs knuffelt hug knuffel yes ja thanks dank} $line ]
   set line [string map -nocase { small kleine wrong fout tea thee cosy muts morning morgen think denk that dat too ook also ook does doet middle midden therefore dus perhaps misschien maybe misschien tree BOOM$(* } $line]
-  set line [string map -nocase { licking likken licks likt for voor fucks paalt fucking paalen dictionary woordenboek dutch nederlands} $line]
+  set line [string map -nocase { licking likken licks likt for voor fucks paalt fucking palen dictionary woordenboek dutch nederlands} $line]
   set line [string map -nocase { honks toetert beeps toetert honk toet} $line]
-
 
 
   #Jeans
@@ -81,7 +78,7 @@ proc bMotion_module_extra_dutchify_makeDutch { line } {
   set line [string map -nocase { sun zon moon maan star ster sky hemel  heaven hemel hell hel ground grond grass gras cloud wolk } $line]
   set line [string map -nocase { bird vogel lion leeuw tiger tijger dog hond cat kat fish vis } $line]
   set line [string map -nocase { dictionary woordenboek word woord translation vertaling } $line]
-  set line [string map -nocase { car auto bike fiets train trein plane vliegtuig truck vrachtwagen boat boot ship schip balloon ballon } $line]
+  set line [string map -nocase { car auto bike fiets train trein {(air)?plane} {vliegtuig} truck vrachtwagen boat boot ship schip balloon ballon } $line]
   set line [string map -nocase { under onder behind achter before voor } $line]
 
   set line [string map -nocase { gives geeft throws gooit tickles kietelt } $line]
@@ -90,10 +87,6 @@ proc bMotion_module_extra_dutchify_makeDutch { line } {
   set line [string map -nocase { not niet talk praten book boek pencil potlood rubber condoom theacup theekopje boat boot } $line]
 
   set line [bMotion_module_extra_dutchify_single_word_replace to naar $line]
-
-  regsub -nocase -all {[[:<:]](ver)(.+?)([auiou])\3([^auiou])(en)[[:>:]]} $line {\1\2\3\4\5} line
-
-  regsub -nocase -all {[[:<:]]([a-z]{4,})ing[[:>:]]} $line {\1end} line
 
   return $line
 }
@@ -106,9 +99,42 @@ proc bMotion_module_extra_dutchify_deBrittify {text} {
   regsub -all "n't\[\[:>:\]\]" $text { not} text
 
   # special case, we don't plural or possesive form (sp?) to be substituted
+  # this will fail with 'has' instead of 'is'
   regsub -all -nocase "\[\[:<:\]\](\[s\]*he)'s\[\[:>:\]\]" $text {\1 is} text
 
   return $text
+}
+
+proc bMotion_module_extra_dutchify_grammar {line} {
+  # some rules in this proc will call makeDutch for small parts of word
+  set subword ""
+
+  #...ed --> ge...[dt]
+  while {[regexp {(\w{3,})ed[[:>:]]} $line discard subword]} {
+    bMotion_putloglev 1 * "subword: $subword"
+    set subword [bMotion_module_extra_dutchify_makeDutch $subword]
+    if {[regexp {(t|k|f|s|ch|p)} [string index $subword end]]} {
+	  if {[regexp {[^t]} [string index $subword end]]} {
+        regsub -nocase {(\w+)ed[[:>:]]} $line "ge${subword}t" line
+	  } else {
+        regsub -nocase {(\w+)ed[[:>:]]} $line "ge${subword}" line
+	  }
+	} else {
+      regsub -nocase {(\w+)ed[[:>:]]} $line "ge${subword}d" line
+    }
+  }
+
+  #...ify -> ver...en
+  regsub -nocase -all {[[:<:]](.+?)ify[[:>:]]} $line {ver\1en} line
+
+  # this has something to do with the above translation, but I can't seem
+  # to figure out what :) It has something to do with removing double vowels
+  regsub -nocase -all {[[:<:]](ver)(.+?)([auiou])\3([^auiou])(en)[[:>:]]} $line {\1\2\3\4\5} line
+
+  # ...ing -> ...end   this fails most of the time :)
+  #regsub -nocase -all {[[:<:]]([a-z]{4,})ing[[:>:]]} $line {\1end} line
+
+  return $line
 }
 
 proc bMotion_module_extra_dutchify {text} {
@@ -123,7 +149,8 @@ proc bMotion_module_extra_dutchify {text} {
   }
 
   set line [bMotion_module_extra_dutchify_deBrittify $line]
-
+  set line [bMotion_module_extra_dutchify_makeDutch_multiWord $line]
+  set line [bMotion_module_extra_dutchify_grammar $line]
   set line [bMotion_module_extra_dutchify_makeDutch $line]
 
   if {$leetMode == 1} {
