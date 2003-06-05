0c22caaa9eb6df89e92bda04ac269cfa29e99410
James Seward <james@jamesoff.net>
2003-06-05 19:05:03 +0000

    new plugin

---
+++ plugins/en/output_pinky.tcl
@@ -0,0 +1,37 @@
+## bMotion output plugin: pinky
+#
+# $Id$
+#
+
+###############################################################################
+# This is a bMotion plugin
+# Copyright (C) James Michael Seward 2000-2002
+#
+# This program is covered by the GPL, please refer the to LICENCE file in the
+# distribution; further information can be found in the headers of the scripts
+# in the modules directory.
+###############################################################################
+
+
+proc bMotion_plugin_output_pinky { channel line } {
+  if {[rand 100] > 95} {
+    set line [string trim $line]
+    if {![regexp "[.!?]$" $line]} {
+      append line "."
+    }
+    append line " %VAR{narfs}"
+
+    set line [bMotionDoInterpolation $line "" "" $channel]
+  }
+  return $line
+}
+
+set narfs {
+  "zort!"
+  "narf"
+  "poit."
+  "poit!"
+  "narf!"
+}
+
+bMotion_plugin_add_output "pinky" bMotion_plugin_output_pinky 1 "all"
