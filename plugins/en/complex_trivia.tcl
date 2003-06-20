eae34e452816d840f19a1d38cd28bfce181770e6
James Seward <james@jamesoff.net>
2003-06-20 15:15:23 +0000

    whoops, typo

--- plugins/en/complex_trivia.tcl
+++ plugins/en/complex_trivia.tcl
@@ -106,7 +106,7 @@ proc bMotion_plugin_complex_trivia_2 { nick host handle channel text } {
   }
   #putlog "elements: $elements, elementcount: $elementcount"
   if {($elements == $elementcount)} {
-    if {$answer != [bMotion-plugins_settings_get "trivia" "last" "" ""]} {
+    if {$answer != [bMotion_plugins_settings_get "trivia" "last" "" ""]} {
       bMotionDoAction $channel $nick $answer
       putloglev d * "answered trivia with $answer"
       bMotion_plugins_settings_set "trivia" "last" "" "" $answer
