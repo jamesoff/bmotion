f4d2d480e9ef2916dc46f994d20738c2d6799e0b
James Seward <james@jamesoff.net>
2003-07-07 15:50:22 +0000

    new plugin

---
+++ plugins/en/complex_woot.tcl
@@ -0,0 +1,31 @@
+## bMotion plugin: bhar (etc)
+#
+# $Id$
+#
+
+###############################################################################
+# This is a bMotion plugin
+# Copyright (C) James Michael Seward 2000-2002
+#
+# This program is covered by the GPL, please refer the to LICENCE file in the
+# distribution; further information can be found in the headers of the scripts
+# in the modules directory.
+###############################################################################
+
+bMotion_plugin_add_complex "woot" {^[a-zA-Z0-9]+[!1~]+$} 5 bMotion_plugin_complex_woot "en"
+
+proc bMotion_plugin_complex_woot { nick host handle channel text } {
+  if [regexp {^([a-zA-Z0-9]+)[!1~]+$} $text matches word] {
+    bMotionDoAction $channel $word "%VAR{woots}"
+  }
+}
+
+set woots {
+  "i like %%"
+  "\\o/"
+  "%REPEAT{3:7} %%"
+  "\\o/ %%"
+  "hurrah"
+  "wh%REPEAR{3:7:e} %%"
+  "%VAR{smiles}"
+}
