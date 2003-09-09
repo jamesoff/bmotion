174d6fcadda0e69bf2f6030305964a4a292f7f76
James Seward <james@jamesoff.net>
2003-09-09 10:13:37 +0000

    new plugin

---
+++ plugins/en/complex_sucks.tcl
@@ -0,0 +1,43 @@
+## bMotion plugin: sucks
+#
+
+# $Id$
+
+#
+
+
+
+###############################################################################
+
+# This is a bMotion plugin
+
+# Copyright (C) James Michael Seward 2000-2002
+
+#
+
+# This program is covered by the GPL, please refer the to LICENCE file in the
+
+# distribution; further information can be found in the headers of the scripts
+
+# in the modules directory.
+
+###############################################################################
+
+
+
+bMotion_plugin_add_complex "sucks" {sucks?$} 30 bMotion_plugin_complex_sucks "en"
+
+
+
+proc bMotion_plugin_complex_sucks { nick host handle channel text } {
+
+  if [regexp -nocase {([^ ]+) ((is|si) (teh|the)? )?sucks?} $text matches item] {
+    bMotionDoAction $channel $item "%VAR{sucks}"
+  }
+}
+
+
+bMotion_abstract_register "sucks"
+bMotion_abstract_add "sucks" "%% = %VAR{PROM}"
+
+
