15f44a072198de9302c3c03c380a08fb53e8c44a
James Seward <james@jamesoff.net>
2003-09-19 15:22:45 +0000

    new plugin

---
+++ plugins/en/action_complex_failsafe.tcl
@@ -0,0 +1,46 @@
+## bMotion plugin: away handler
+
+#
+
+# $Id$
+
+#
+
+
+
+###############################################################################
+
+# This is a bMotion plugin
+
+# Copyright (C) James Michael Seward 2000-2002
+
+#
+
+# This program is covered by the GPL, please refer the to LICENCE file in the
+
+# distribution; further information can be found in the headers of the scripts
+
+# in the modules directory.
+
+###############################################################################
+
+
+
+bMotion_plugin_add_action_complex "zzz-failsafe" {^[^ s]+s %botnicks} 100 bMotion_plugin_complex_action_failsafe "en"
+
+
+
+proc bMotion_plugin_complex_action_failsafe { nick host handle channel text } {
+
+  regexp {^([^ s]+)s } $text matches verb
+
+  if {$verb == ""} {
+    return 1
+  }
+
+  bMotionDoAction $channel $verb "%VAR{failsafes}"
+}
+
+
+bMotion_abstract_register "failsafes"
+bMotion_abstract_batchadd "failsafes" [list "%VAR{rarrs}" "%REPEAT{3:7:m} %%" "%VAR{thanks}"]
