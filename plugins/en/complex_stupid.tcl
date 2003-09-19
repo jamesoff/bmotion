493a9667479b64a5b670320c71aaad84619b47b2
James Seward <james@jamesoff.net>
2003-09-19 15:50:23 +0000

    more likely to notice

--- plugins/en/complex_stupid.tcl
+++ plugins/en/complex_stupid.tcl
@@ -1,25 +1,49 @@
 ## bMotion plugin: stupid bots
-#
-# $Id$
+
 #
 
-###############################################################################
-# This is a bMotion plugin
-# Copyright (C) James Michael Seward 2000-2002
+# $Id$
+
 #
-# This program is covered by the GPL, please refer the to LICENCE file in the
-# distribution; further information can be found in the headers of the scripts
-# in the modules directory.
+
+
+
 ###############################################################################
+
+# This is a bMotion plugin
+
+# Copyright (C) James Michael Seward 2000-2002
+
+#
+
+# This program is covered by the GPL, please refer the to LICENCE file in the
+
+# distribution; further information can be found in the headers of the scripts
+
+# in the modules directory.
+
+###############################################################################
+
+
 
 bMotion_plugin_add_complex "stupid" "(stupid|idiot|imbecile|incompetent|loser|luser) bot" 100 bMotion_plugin_complex_stupid "en"
 
+
+
 proc bMotion_plugin_complex_stupid { nick host handle channel text } {
+
   global botnicks
-  if {[regexp -nocase "(stupid|idiot|imbecile|incompetent|loser|luser) bot" $text] && [regexp -nocase $botnicks $text]} {
+
+  if {[regexp -nocase "(stupid|idiot|imbecile|incompetent|loser|luser)( bot)?" $text] && [regexp -nocase $botnicks $text]} {
+
     bMotionDoAction $channel [bMotionGetRealName $nick $host] "%VAR{stupidReplies}"
+
     bMotionGetSad
+
     driftFriendship $nick -5
+
     return 0
+
   }
+
 }
