4b94ba9dee5f9d0ee90237509b8068258e7b30b8
James Seward <james@jamesoff.net>
2003-09-22 13:46:05 +0000

    more intelligent handling of "-ing" words

--- plugins/en/complex_want_catch.tcl
+++ plugins/en/complex_want_catch.tcl
@@ -1,42 +1,88 @@
 ## bMotion plugin: want catcher
-#
-# Jolly sneaky.. if someone wants something, we'll remember it for ourselves :)
-#
-# $Id$
+
 #
 
-###############################################################################
-# This is a bMotion plugin
-# Copyright (C) James Michael Seward 2000-2002
+# Jolly sneaky.. if someone wants something, we'll remember it for ourselves :)
+
 #
-# This program is covered by the GPL, please refer the to LICENCE file in the
-# distribution; further information can be found in the headers of the scripts
-# in the modules directory.
+
+# $Id$
+
+#
+
+
+
 ###############################################################################
+
+# This is a bMotion plugin
+
+# Copyright (C) James Michael Seward 2000-2002
+
+#
+
+# This program is covered by the GPL, please refer the to LICENCE file in the
+
+# distribution; further information can be found in the headers of the scripts
+
+# in the modules directory.
+
+###############################################################################
+
+
 
 bMotion_plugin_add_complex "want-catch" "i (want|need) (.+)" 100 bMotion_plugin_complex_want_catcher "en"
+
 bMotion_plugin_add_complex "mmm-catch" "^mmm+ (.+)" 100 bMotion_plugin_complex_mmm_catcher "en"
+
 bMotion_plugin_add_complex "noun-catch" {[[:<:]](?:a|an|the) ([[:alpha:]]+)} 100 bMotion_plugin_complex_noun_catcher "en"
 
+
 proc bMotion_plugin_complex_want_catcher { nick host handle channel text } {
+
   if [regexp -nocase "i (want|need) (?!to)(.+? )" $text matches verb item] {
+
     #that's a negative lookahead ---^
+
     bMotion_flood_undo $nick
+
     bMotion_abstract_add "sillyThings" $item
   }
+
 }
+
 
 proc bMotion_plugin_complex_mmm_catcher { nick host handle channel text } {
+
   if [regexp -nocase "^mmm+ (.+?) " $text matches item] {
+
     bMotion_flood_undo $nick
+
     bMotion_abstract_add "sillyThings" $item
   }
+
 }
 
+
+
 proc bMotion_plugin_complex_noun_catcher { nick host handle channel text } {
-  if [regexp -nocase {[[:<:]](a|an|the|some) ([[:alpha:]]+(?!ed|ing|ce|ly))} $text matches prefix item] {
+
+  if [regexp -nocase {[[:<:]](a|an|the|some) ([[:alpha:]]+)( [[:alpha:]]+[[:>:]])?} $text matches prefix item second] {
+
     bMotion_flood_undo $nick
+
     set item [string tolower $item]
+
+    if [regexp "(ly)$" $item] {
+      return 0
+    }
+    
+    if [regexp "(ing|ed)$" $item] {
+      if {$second == ""} {
+        return 0
+      }
+      append item $second
+    }
+
     set prefix [string tolower $prefix]
     if {$prefix == "the"} {
       if {[string range $item end end] == "s"} {
