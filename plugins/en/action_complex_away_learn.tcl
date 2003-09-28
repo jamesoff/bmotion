ddad524987975a46d5fb8f3286920025b1d5a52a
James Seward <james@jamesoff.net>
2003-09-28 17:58:07 +0000

    new plugin to let bmotion learn its own away reasons

---
+++ plugins/en/action_complex_away_learn.tcl
@@ -0,0 +1,33 @@
+## bMotion plugin: away reason learning
+#
+# $Id$
+#
+
+###############################################################################
+# This is a bMotion plugin
+# Copyright (C) James Michael Seward 2000-2002
+#
+# This program is covered by the GPL, please refer the to LICENCE file in the
+# distribution; further information can be found in the headers of the scripts
+# in the modules directory.
+###############################################################################
+
+bMotion_plugin_add_action_complex "away-learn" "^(is )?(away|gone)" 50 bMotion_plugin_complex_action_away_learn "en"
+
+#make sure this abstract is registered
+bMotion_abstract_register "randomAways"
+
+proc bMotion_plugin_complex_action_away_learn { nick host handle channel text } {
+
+  #autoaway
+  if [regexp -nocase {(auto[ -]?away)|idle} $text] {
+    #don't want to learn this
+    return 0
+  }
+
+  if [regexp {^(is )?(away|gone)[^a-z]*([a-z0-9 ]+)} $text matches skip1 skip2 reason] {
+    bMotion_putloglev d * "learning away reason: $reason"
+    bMotion_abstract_add "randomAways" [string tolower $reason]
+  }
+  return 0
+}
