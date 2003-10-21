86a7a87c9b5e56a65837df968713197871a72f45
Kevin Smith <no@email.address>
2003-10-21 15:27:29 +0000

    question handler for feelings check now cares (a little) about if bmotion likes you

--- modules/extra/question.tcl
+++ modules/extra/question.tcl
@@ -19,56 +19,80 @@ proc bMotion_question_wellbeing { nick channel host } {
     global mood
     driftFriendship $nick 2
 
+    set moodString ""
     #if {![bMotionTalkingToMe $text]} { return 0 }
+    if {[getFriendship $nick] > 75} {
+	append moodString "Awww, thanks for asking "
+	append moodString [bMotionGetRealName $nick]
+	append moodString ", "
+    } else {
+	if {[getFriendship $nick] < 35} {
+	    append moodString "What do you care that "
+	}
+    }
+    
 
-    set moodString "I'm feeling "
+    append moodString "I'm feeling "
     set moodIndex 0
+    set done 0
+
+    bMotion_putloglev 4 * "lonely $mood(lonely)"    
     if {$mood(lonely) > 5} {
 	append moodString "a bit lonely"
 	incr moodIndex -2
+	incr done 1
     }
     
+    bMotion_putloglev 4 * "horny $mood(horny)"
     if {$mood(horny) > 2} {
-	if {[string length $moodString] > 13} {
+	if {$done > 0} {
 	    append moodString ", "
 	}
 	append moodString "a little horny"
 	incr moodIndex 2
+	incr done 1
     }
-    
+
+    bMotion_putloglev 4 * "happy $mood(happy)"        
     if {$mood(happy) > 3} {
-	if {[string length $moodString] > 13} {
+	if {$done > 0} {
 	    append moodString ", "
 	}
 	append moodString "happy"
 	incr moodIndex 1
+	incr done 1
     }
-    
+
+
     if {$mood(happy) < 0} {
-	if {[string length $moodString] > 13} {
+	if {$done > 0} {
 	    append moodString ", "
 	}
 	append moodString "sad"
 	incr moodIndex -3
+	incr done 1
     }
-    
+
+    bMotion_putloglev 4 * "stoned $mood(stoned)"    
     if {$mood(stoned) > 5} {
-	if {[string length $moodString] > 13} {
+	if {$done > 0} {
 	    append moodString ", "
 	}
 	append moodString "stoned off my tits"
 	incr moodIndex 2
+	incr done 1
     }
     
-    if {$moodIndex >= 0} {
-	append moodString " :)"
+    if {$done < 1} {
+	append moodString "pretty average :/"
+# 	global feelings
+# 	set moodString [bMotion_abstract_get "feelings"]
     } else {
-	append moodString " :("
-    }
-
-    if {[string length $moodString] == [string length "I'm feeling  :)"]} {
-	global feelings
-	set moodString [bMotion_abstract_get "feelings"]
+	if {$moodIndex >= 0} {
+	    append moodString " :)"
+	} else {
+	    append moodString " :("
+	}
     }
     
     bMotionDoAction $channel [bMotionGetRealName $nick $host] "%%: $moodString"
