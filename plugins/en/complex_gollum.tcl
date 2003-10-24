839061e5e06342aa949ee2737a8c5993ffbf51b5
Kevin Smith <no@email.address>
2003-10-24 14:23:27 +0000

    putlogs put in gollum

--- plugins/en/complex_gollum.tcl
+++ plugins/en/complex_gollum.tcl
@@ -15,9 +15,14 @@
 bMotion_plugin_add_complex "gollum" "precious" 50 bMotion_plugin_complex_gollum "en"
 
 proc bMotion_plugin_complex_gollum { nick host handle channel text } {
-    if {![bMotion_interbot_me_next $channel]} { return 0 }
+    bMotion_putloglev 2 * "bMotion: run complex_gollum,"
+    if {![bMotion_interbot_me_next $channel]} { 
+	bMotion_putloglev 4 * "bMotion: would have run gollum, but not my turn"
+	return 0 
+    }
+    bMotion_putloglev 4 * "bMotion: my turn when running complex_gollum"
     bMotionDoAction $channel [bMotionGetRealName $nick $host] "%VAR{gollums}"
-    return 1
+    return 0 #this allows us to still respond, right?
   }
 }
 
