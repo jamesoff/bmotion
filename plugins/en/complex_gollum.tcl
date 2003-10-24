533a7184c5200b349f20109f003a169cb79c315d
Kevin Smith <no@email.address>
2003-10-24 14:10:53 +0000

    more gollumssses

--- plugins/en/complex_gollum.tcl
+++ plugins/en/complex_gollum.tcl
@@ -1,6 +1,6 @@
 ## bMotion plugin: gollum
 #
-# $Id: 
+# 	$Id$	
 #
 
 ###############################################################################
@@ -21,17 +21,4 @@ proc bMotion_plugin_complex_gollum { nick host handle channel text } {
   }
 }
 
-set gollums {
-    "Foolishness!"
-    "Stupid fat hobbit!"
-    "We hates the fat hobbit! Him and his pots and pans and ropes."
-    "Thieves. Thieves all!"
-    "Stupid hobbit. Talking to himself like that. Not answering himself."
-    "Yes. Sleep sweet sleep with our precious!"
-    "Filthy little thief playing with our precious!"
-    "If only fat hobbit would go away for a moment, we could go to master and take the precious away from him."
-    "No! Must not take away our precious!"
-    "We miss the song of the precious. We miss it. We wants it back!"
-    "Everyone wants my precious. It's not fair! It's ours!" 
-    "Shiny, shiny."
-}
+
