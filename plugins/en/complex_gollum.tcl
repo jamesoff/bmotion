034d58157bd4a21b37e698849226a3404b672031
Kevin Smith <no@email.address>
2003-11-02 00:05:47 +0000

    moved gollum abstract from abstracts to gollum :)

--- plugins/en/complex_gollum.tcl
+++ plugins/en/complex_gollum.tcl
@@ -22,8 +22,10 @@ proc bMotion_plugin_complex_gollum { nick host handle channel text } {
     }
     bMotion_putloglev 4 * "bMotion: my turn when running complex_gollum"
     bMotionDoAction $channel [bMotionGetRealName $nick $host] "%VAR{gollums}"
-    return 0 #this allows us to still respond, right?
+    return 0 
+    #this allows us to still respond to questions, right?
   }
 }
 
-
+bMotion_abstract_register "gollums"
+bMotion_abstract_batchadd "gollums" [list "Foolishness!" "Stupid fat hobbit!" "We hates the fat hobbit! Him and his pots and pans and ropes." "Thieves. Thieves all!" "Stupid hobbit. Talking to himself like that. Not answering himself." "Yes. Sleep sweet sleep with our precious!" "Filthy little thief playing with our precious!" "If only fat hobbit would go away for a moment, we could go to master and take the precious away from him." "No! Must not take away our precious!" "We miss the song of the precious. We miss it. We wants it back!" "Everyone wants my precious. It's not fair! It's ours!" "Shiny, shiny." ]
