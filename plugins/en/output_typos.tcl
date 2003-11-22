4b613fff842785656428e0d76837324ade4002c0
James Seward <james@jamesoff.net>
2003-11-22 01:43:40 +0000

    apply typos per word not per line

--- plugins/en/output_typos.tcl
+++ plugins/en/output_typos.tcl
@@ -13,8 +13,113 @@
 ###############################################################################
 
 
-#note: if you write your own typos plugin that does this sort of thing, call it "typos" 
+#note: if you write your own typos plugin that does this sort of thing, call it "typos"
 #      too because loading two plugins to simulate typing errors is meaningless (mostly)
+
+proc bMotion_plugin_output_typos_do { line } {
+  if {[rand 1000] <= $typochance} {
+    set line [string map -nocase { is si ome oem ame aem oe eo } $line ]
+    set typochance [expr $typochance * 0.6]
+  }
+
+  if {[rand 1000] <= $typochance} {
+    set line [string map -nocase { aid iad ers ars ade aed ite eit } $line ]
+    set typochance [expr $typochance * 0.6]
+  }
+
+  if {[rand 1000] <= $typochance} {
+    set line [string map -nocase { hi ih or ro ip pi ho oh } $line ]
+    set typochance [expr $typochance * 0.6]
+  }
+
+  if {[rand 1000] <= $typochance} {
+    set line [string map -nocase { he eh re er in ni lv vl sec sex } $line ]
+    set typochance [expr $typochance * 0.6]
+  }
+
+  if {[rand 1000] <= $typochance} {
+    set line [string map -nocase { ir ri ou uo ha ah ui iu ig gi nd dn} $line ]
+    set typochance [expr $typochance * 0.6]
+  }
+
+#  if {[rand 1000] <= $typochance} {
+#    set line [string map -nocase { " t" "t " } $line ]
+#    set typochance [expr $typochance * 0.6]
+#  }
+
+  #go though the line one char at a time
+  set chars [split $line {}]
+  set newLine ""
+  set typochance [expr $bMotionSettings(typos) / 2]
+  foreach char $chars {
+    if [string match -nocase "l" $char] {
+      if {[rand 1000] < $typochance} {
+        append newLine ";l"
+        bMotion_plugin_output_typos_adderror "" "-;"
+        continue
+      }
+    }
+
+    if [string match -nocase "a" $char] {
+      if {[rand 1000] < $typochance} {
+        append newLine "sa"
+        bMotion_plugin_output_typos_adderror "" "-s"
+        continue
+      }
+    }
+    if [string match -nocase "s" $char] {
+      if {[rand 1000] < $typochance} {
+        append newLine "sd"
+        bMotion_plugin_output_typos_adderror "" "-d"
+        continue
+      }
+    }
+    if [string match -nocase "e" $char] {
+      if {[rand 1000] < $typochance} {
+        append newLine "re"
+        bMotion_plugin_output_typos_adderror "" "-r"
+        continue
+      }
+    }
+    if [string match -nocase "d" $char] {
+      if {[rand 1000] < $typochance} {
+        append newLine "df"
+        bMotion_plugin_output_typos_adderror "" "-f"
+        continue
+      }
+    }
+    if [string match -nocase "z" $char] {
+      if {[rand 1000] < $typochance} {
+        append newLine "zx"
+        bMotion_plugin_output_typos_adderror "" "-x"
+        continue
+      }
+    }
+    if [string match -nocase "z" $char] {
+      if {[rand 1000] < $typochance} {
+        append newLine "z\\"
+        bMotion_plugin_output_typos_adderror "" "-\\"
+        continue
+      }
+    }
+    if [string match -nocase " " $char] {
+      if {[rand 1000] < $typochance} {
+        bMotion_plugin_output_typos_adderror "" "+space"
+        continue
+      }
+    }
+    if [string match -nocase ")" $char] {
+      if {[rand 1000] < $typochance} {
+        append newLine ")_"
+        bMotion_plugin_output_typos_adderror "" "-_"
+        continue
+      }
+    }
+    #else...
+    append newLine $char
+  }
+  return $newLine
+}
 
 ## Make Typos
 #    Attempt to make typos similar to human typing errors
@@ -29,115 +134,25 @@ proc bMotion_plugin_output_typos { channel line } {
   bMotion_plugins_settings_set "output:typos" "typos" "" "" ""
   bMotion_plugins_settings_set "output:typos" "typosDone" "" "" ""
 
-  if {[rand 1000] <= $typochance} { 
-    set line [string map -nocase { is si ome oem ame aem oe eo } $line ]
-    set typochance [expr $typochance * 0.6] 
-  }
-
-  if {[rand 1000] <= $typochance} { 
-    set line [string map -nocase { aid iad ers ars ade aed ite eit } $line ]
-    set typochance [expr $typochance * 0.6]
-  }
-
-  if {[rand 1000] <= $typochance} { 
-    set line [string map -nocase { hi ih or ro ip pi ho oh "ine " "ing " } $line ]
-    set typochance [expr $typochance * 0.6]
-  }
-
-  if {[rand 1000] <= $typochance} { 
-    set line [string map -nocase { he eh re er in ni lv vl sec sex } $line ] 
-    set typochance [expr $typochance * 0.6]
-  }
-
-  if {[rand 1000] <= $typochance} { 
-    set line [string map -nocase { ir ri ou uo ha ah ui iu ig gi nd dn} $line ]
-    set typochance [expr $typochance * 0.6]
-  }
-
-  if {[rand 1000] <= $typochance} { 
-    set line [string map -nocase { " t" "t " } $line ]
-    set typochance [expr $typochance * 0.6]
-  }
-
-  #go though the line one char at a time
-  set chars [split $line {}]
+  #split words
+  set words [split $line {}]
   set newLine ""
-  set typochance [expr $bMotionSettings(typos) / 2]
-  foreach char $chars {
-    if [string match -nocase "l" $char] { 
-      if {[rand 1000] < $typochance} { 
-        append newLine ";l"
-        bMotion_plugin_output_typos_adderror "" "-;"
-        continue 
-      }
-    }    
-    if [string match -nocase "a" $char] { 
-      if {[rand 1000] < $typochance} { 
-        append newLine "sa"
-        bMotion_plugin_output_typos_adderror "" "-s"
-        continue 
-      }
-    }
-    if [string match -nocase "s" $char] { 
-      if {[rand 1000] < $typochance} { 
-        append newLine "sd"
-        bMotion_plugin_output_typos_adderror "" "-d"
-        continue
-      }
-    }
-    if [string match -nocase "e" $char] { 
-      if {[rand 1000] < $typochance} { 
-        append newLine "re"
-        bMotion_plugin_output_typos_adderror "" "-r"
-        continue
-      }
-    }
-    if [string match -nocase "d" $char] { 
-      if {[rand 1000] < $typochance} { 
-        append newLine "df"
-        bMotion_plugin_output_typos_adderror "" "-f"
-        continue
-      }
-    }
-    if [string match -nocase "z" $char] { 
-      if {[rand 1000] < $typochance} { 
-        append newLine "zx"
-        bMotion_plugin_output_typos_adderror "" "-x"
-        continue
-      }
-    }
-    if [string match -nocase "z" $char] { 
-      if {[rand 1000] < $typochance} {
-        append newLine "z\\"
-        bMotion_plugin_output_typos_adderror "" "-\\"
-        continue
-      }
-    }
-    if [string match -nocase " " $char] { 
-      if {[rand 1000] < $typochance} { 
-        bMotion_plugin_output_typos_adderror "" "+space"
-        continue
-      }
-    }
-    if [string match -nocase ")" $char] { 
-      if {[rand 1000] < $typochance} { 
-        append newLine ")_"
-        bMotion_plugin_output_typos_adderror "" "-_"
-        continue
-      }
-    }
-    #else...
-    append newLine $char
+
+  #typo words
+  foreach word $words {
+    set word [bMotion_plugin_output_typos_do $word]
+    append newLine "$word "
   }
-  set line $newLine
-  
-  if {[rand 1000] < $typochance} { 
+
+  set line [string trim $newLine]
+
+  if {[rand 1000] < $typochance} {
     set tmpchar [pickRandom {"#" "]"}]
     append line $tmpchar
     bMotion_plugin_output_typos_adderror "" "-$tmpchar"
   }
 
-  if {[rand 1000] < $typochance} { 
+  if {[rand 1000] < $typochance} {
     set line [string toupper $line]
     bMotion_plugin_output_typos_adderror "" "-caps"
   }
