77ca7ff9ab1f3d62e152722d4d502221f8f1017d
James Seward <james@jamesoff.net>
2004-01-03 14:16:46 +0000

    fixed wrongly named proc

--- plugins/en/complex_shocked.tcl
+++ plugins/en/complex_shocked.tcl
@@ -1,16 +1,29 @@
 # $Id$
+
 #
 
+
+
 ###############################################################################
+
 # This is a bMotion plugin
+
 # Copyright (C) James Michael Seward 2000-2002
+
 #
+
 # This program is covered by the GPL, please refer the to LICENCE file in the
+
 # distribution; further information can be found in the headers of the scripts
+
 # in the modules directory.
+
 ###############################################################################
 
-proc bMotion_plugin_complex_shocked { nick host handle channel text } {
+
+
+proc bMotion_plugin_complex_shock { nick host handle channel text } {
+
   global bMotionCache
   #if we spoke last, it's probable this is a reaction to us
   #being surprised at yourself is lame
@@ -23,3 +36,4 @@ proc bMotion_plugin_complex_shocked { nick host handle channel text } {
 }
 
 bMotion_plugin_add_complex "shocked" "^((((=|:|;)-?(o|0))|(!+))|blimey|crumbs|i say)$" 40 bMotion_plugin_complex_shock "en"
+
