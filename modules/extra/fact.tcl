b1f1ecc45a5b3ebff22623493874b2a03ec841c2
James Seward <james@jamesoff.net>
2004-02-15 13:43:05 +0000

    auto-tidy up if too many facts

--- modules/extra/fact.tcl
+++ modules/extra/fact.tcl
@@ -1,27 +1,23 @@
 # bMotion facts module
-
 # $Id$
 
 ###############################################################################
-
 # This is a bMotion plugin
-
 # Copyright (C) James Michael Seward 2000-2002
-
 #
-
 # This program is covered by the GPL, please refer the to LICENCE file in the
-
 # distribution; further information can be found in the headers of the scripts
-
 # in the modules directory.
-
 ###############################################################################
 
+# maximum number of things about which facts can be known
+set bMotion_facts_max_items 500
+
+# maximum number of facts to know about an item
+set bMotion_facts_max_facts 20
 
 # initialise
 if {![info exists bMotionFacts]} {
-
   set bMotionFacts(what,bmotion) [list "a very nice script"]
 }
 
@@ -77,18 +73,49 @@ proc bMotion_facts_load { } {
 
 proc bMotion_facts_save { } {
   global bMotionFacts bMotionModules
+  global bMotion_facts_max_facts
+  global bMotion_facts_max_items
+
+  set tidy 0
+  set tidyfact 0
+  set count 0 
 
   bMotion_putloglev 1 * "Saving facts to $bMotionModules/facts/facts.txt"
 
   set fileHandle [open "$bMotionModules/facts/facts.txt" "w"]
 
   set items [array names bMotionFacts]
+  if {[llength $items] > $bMotion_facts_max_items} {
+    putlog "Too many items are known ([llength $items] > $bMotion_facts_max_items), tidying up"
+    set tidy 1
+  }
   foreach item $items {
+    if {$tidy} {
+      if {[rand 100]< 10} {
+        #clear array entry
+        unset bMotionFacts($item)
+        incr count
+        continue
+      }
+    }
+    if {[llength $bMotionFacts($item)] > $bMotion_facts_max_facts} {
+      set tidyfact 1
+    } else {
+      set tidyfact 0
+    }
     foreach fact $bMotionFacts($item) {
-      puts $fileHandle "$item $fact"
+      if {$tidyfact} {
+        if {[rand 100] < 10} {
+          #less critical so we won't waste time trying to delete from memory too :)
+          continue
+        }
+        puts $fileHandle "$item $fact"
+      }
     }
   }
-
+  if {$tidy} {
+    putlog "$count facts have been forgo.. los... delet... thingy *dribbles*"
+  }
   close $fileHandle
 }
 
