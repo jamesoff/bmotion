82a21a38055c8d37f14f459079baf14217a77344
James Seward <james@jamesoff.net>
2004-03-01 10:50:57 +0000

    fixed missing-first-letter bug

--- plugins/en/action_complex_away_learn.tcl
+++ plugins/en/action_complex_away_learn.tcl
@@ -18,16 +18,16 @@ bMotion_plugin_add_action_complex "away-learn" "^(is )?(away|gone)" 50 bMotion_p
 bMotion_abstract_register "randomAways"
 
 proc bMotion_plugin_complex_action_away_learn { nick host handle channel text } {
-
   #autoaway
   if [regexp -nocase {(auto[ -]?away)|idle} $text] {
     #don't want to learn this
     return 0
   }
 
-  if [regexp {^(is )?(away|gone)[^a-z]*([a-z0-9 ]+)} $text matches skip1 skip2 reason] {
+  if [regexp -nocase {^(is )?(away|gone)[^a-z0-9]*([a-z0-9 ]+)} $text matches skip1 skip2 reason] {
     bMotion_putloglev d * "learning away reason: $reason"
     bMotion_abstract_add "randomAways" [string tolower $reason]
   }
+
   return 0
 }
