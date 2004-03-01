2de69359dcfa3ddb2d4a82ef1cae7e516cd276d1
James Seward <james@jamesoff.net>
2004-03-01 10:51:50 +0000

    fixed linebreaks

--- plugins/en/action_complex_failsafe.tcl
+++ plugins/en/action_complex_failsafe.tcl
@@ -1,39 +1,23 @@
 ## bMotion plugin: away handler
-
 #
-
 # $Id$
-
 #
 
 
 
 ###############################################################################
-
 # This is a bMotion plugin
-
 # Copyright (C) James Michael Seward 2000-2002
-
 #
-
 # This program is covered by the GPL, please refer the to LICENCE file in the
-
 # distribution; further information can be found in the headers of the scripts
-
 # in the modules directory.
-
 ###############################################################################
-
-
-
 
 bMotion_plugin_add_action_complex "zzz-failsafe" {^(.+?)s %botnicks} 100 bMotion_plugin_complex_action_failsafe "en"
 
-
 proc bMotion_plugin_complex_action_failsafe { nick host handle channel text } {
-
   regexp {^(.+?)(e?(s)) } $text matches verb ee es
-
   if {$verb == ""} {
     return 1
   }
@@ -49,4 +33,4 @@ proc bMotion_plugin_complex_action_failsafe { nick host handle channel text } {
 
 
 bMotion_abstract_register "failsafes"
-bMotion_abstract_batchadd "failsafes" [list "%VAR{rarrs}" "%REPEAT{3:7:m} %%" "%VAR{thanks}" "i do love a good %%ing" "/%%s %SETTING{complex:failsafe:last:nick:moo} back with %VAR{sillyThings}"]
+bMotion_abstract_batchadd "failsafes" [list "%VAR{rarrs}" "%REPEAT{3:7:m} %%" "%VAR{thanks}" "i do love a good %%ing" "/%%s %SETTING{complex:failsafe:last:nick:moo} back with %VAR{sillyThings}" "what"]
