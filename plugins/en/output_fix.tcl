48c22aed5b341c5cc5a189ee4f0d97ecaeace05b
James Seward <james@jamesoff.net>
2004-03-26 13:52:54 +0000

    fix broken abstracts

---
+++ plugins/en/output_fix.tcl
@@ -0,0 +1,25 @@
+## bMotion output plugin: colloquial
+#
+# $Id$
+#
+
+###############################################################################
+# This is a bMotion plugin
+# Copyright (C) James Michael Seward 2000-2002
+#
+# This program is covered by the GPL, please refer the to LICENCE file in the
+# distribution; further information can be found in the headers of the scripts
+# in the modules directory.
+###############################################################################
+
+bMotion_plugin_add_output "fix" bMotion_plugin_output_fix 1 "all"
+
+
+#  Fix broken output
+#
+proc bMotion_plugin_output_fix { channel line } {
+  regsub -nocase {\$var} $line "%VAR" line
+  regsub -nocase {%pickuser[^%]+%\|} $line "" line
+  set line [bMotionDoInterpolation $line "" "" $channel]
+  return $line
+}
