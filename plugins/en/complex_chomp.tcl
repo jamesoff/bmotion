9bbdd1b6f4c990adcf1fb2fe34884e34c3d7911a
James Seward <james@jamesoff.net>
2004-09-01 20:38:18 +0000

    new plugin: chomp

---
+++ plugins/en/complex_chomp.tcl
@@ -0,0 +1,30 @@
+## bMotion plugin: chomp
+#
+# $Id$
+
+###############################################################################
+# This is a bMotion plugin
+# Copyright (C) James Michael Seward 2000-2002
+#
+# This program is covered by the GPL, please refer the to LICENCE file in the
+# distribution; further information can be found in the headers of the scripts
+# in the modules directory.
+###############################################################################
+
+bMotion_plugin_add_complex "chomp" "^%botnicks,?:? ?(please )?chomp" 100 bMotion_plugin_complex_chomp "en"
+
+proc bMotion_plugin_complex_chomp { nick host handle channel text } {  
+  global botnicks
+  if [regexp -nocase "^$botnicks,?:? ?(please )?chomp (.+)" $text matches ming ming2 who] {
+    global bMotionInfo
+    if {![regexp -nocase $botnicks $who]} {
+      bMotionDoAction $channel $nick "/bites $who's %VAR{bodypart}"
+      bMotionGetUnLonely
+    } else {
+      bMotionDoAction $channel "" "/looks at suspiciously at $nick"
+      bMotionDoAction $channel "" "Do I look like Ouroboros?!"
+      driftFriendship $nick -1
+    }
+    return 1
+  }
+}
