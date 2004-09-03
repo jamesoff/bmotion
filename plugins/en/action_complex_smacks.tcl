aa7ed11b1fa34e11da6854677f49856edadea682
James Seward <james@jamesoff.net>
2004-09-03 15:05:46 +0000

    fixed trigger regexp taunt people using troutslaps

--- plugins/en/action_complex_smacks.tcl
+++ plugins/en/action_complex_smacks.tcl
@@ -11,19 +11,33 @@
 # in the modules directory.
 ###############################################################################
 
-bMotion_plugin_add_action_complex "smacks" "^attacks %botnicks" 100 bMotion_plugin_complex_action_smacks "en"
+bMotion_plugin_add_action_complex "smacks" "^(kicks|smacks|twats|injures|beats up|punches|hits|thwaps|slaps|pokes|kills|destroys) %botnicks" 100 bMotion_plugin_complex_action_smacks "en"
 
 proc bMotion_plugin_complex_action_smacks { nick host handle channel text } {
   global botnicks
   if [regexp -nocase "(kicks|smacks|twats|injures|beats up|punches|hits|thwaps|slaps|pokes|kills|destroys) ${botnicks}" $text] {
+  	if [regexp -nocase "slaps $botnicks around( a bit)? with a( large)? trout" $text] {
+  		bMotionDoAction $channel $nick "%VAR{trouts}"
+  		return 1
+  	}
     bMotionGetUnHappy
     bMotionGetUnLonely
     driftFriendship $nick -2
     if [rand 2] {
-      frightened $nick $dest
+      frightened $nick $channel
       return 1
     }
-    bMotionDoAction $dest $nick "/%VAR{smacks} %% back with %VAR{sillyThings}"
+    bMotionDoAction $channel $nick "/%VAR{smacks} %% back with %VAR{sillyThings}"
     return 1
   }
+}
+
+bMotion_abstract_register "trouts"
+bMotion_abstract_batchadd "trouts" {
+	"/slaps %% back using a default menu command"
+	"%VAR{goAways}"
+	"omg n00b"
+	"omg noob"
+	"n00b"
+	"%VAR{goAways} noob"
 }
