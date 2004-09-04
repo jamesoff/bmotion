d67eae722e79805a4010898a520ecac4ab13cc06
James Seward <james@jamesoff.net>
2004-09-04 19:22:48 +0000

    drop the bmadmin version

--- plugins/admin_flood.tcl
+++ plugins/admin_flood.tcl
@@ -12,46 +12,12 @@
 # in the modules directory.
 ###############################################################################
 
-proc bMotion_plugin_admin_flood { handle idx { arg "" }} {
-  global bMotion_flood_info bMotion_flood_last bMotion_flood_lasttext bMotion_flood_undo
- 
-  #flood show <handle>
-  if [regexp -nocase {show ([^ ]+)} $arg matches handle] {
-    putidx $idx "bMotion: Flood for $handle is [bMotion_flood_get $handle]\r"
-    return 0
-  }
-
-  #flood set <handle> <n>
-  if [regexp -nocase {set ([^ ]+) (.+)} $arg matches handle value] {
-    set bMotion_flood_info($handle) $value
-    putidx $idx "bMotion: flood for $handle set to 0\r"
-    return 0
-  }
-
-
-  #status
-  if [regexp -nocase {status} $arg] {
-    set handles [array names bMotion_flood_info]
-    putidx $idx "bMotion: current flood info:\r"
-    foreach handle $handles {
-      putidx $idx "$handle: [bMotion_flood_get $handle]\r"
-    }
-    return 0
-  }
-
-  #all else fails, list help
-  putidx $idx ".bmadmin flood \[show|set|status\]\r"
-  return 0
-}
-
-# register the plugin
-bMotion_plugin_add_admin "flood" "^flood" n "bMotion_plugin_admin_flood" "any"
 
 ### management version
 
 proc bMotion_plugin_management_flood { handle { arg "" } } {
   global bMotion_flood_info bMotion_flood_last bMotion_flood_lasttext bMotion_flood_undo
- 
+
   #flood show <handle>
   if [regexp -nocase {show ([^ ]+)} $arg matches handle] {
     bMotion_putadmin "bMotion: Flood for $handle is [bMotion_flood_get $handle]"
