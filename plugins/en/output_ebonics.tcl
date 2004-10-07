27bfa65eddded38081d2636cfbae057e5e22d1e2
James Seward <james@jamesoff.net>
2004-10-07 11:39:17 +0000

    new plugin

---
+++ plugins/en/output_ebonics.tcl
@@ -0,0 +1,36 @@
+## bMotion output plugin: english
+#
+# $Id$
+#
+
+###############################################################################
+# This is a bMotion plugin
+# Copyright (C) James Michael Seward 2000-2002
+#
+# This program is covered by the GPL, please refer the to LICENCE file in the
+# distribution; further information can be found in the headers of the scripts
+# in the modules directory.
+###############################################################################
+
+bMotion_plugin_add_output "ebonics" bMotion_plugin_output_ebonics 0 "en"
+
+
+proc bMotion_plugin_output_ebonics { channel line } {
+  set line [string trim $line]
+
+  set newline ""
+  set words [split $line " "]
+
+  foreach word $words {
+		if [regexp -nocase {^([aeiou]?[^aeiou]+[aeiou])\w{2,}$} $word matches first] {
+			set word "${first}zzle"
+		} 
+
+		set word [string map -nocase {for fo} $word]
+
+		append newline "$word "
+		
+  }
+
+  return $newline
+}
