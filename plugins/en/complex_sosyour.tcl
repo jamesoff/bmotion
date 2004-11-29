68ab68eb7a030ec61bc2bb42c739c1bb9e57914f
James Seward <james@jamesoff.net>
2004-11-29 18:30:23 +0000

    new plugin

---
+++ plugins/en/complex_sosyour.tcl
@@ -0,0 +1,38 @@
+## bMotion so's your face plugin
+#
+# $Id$
+#
+
+###############################################################################
+# This is a bMotion plugin
+# Copyright (C) James Michael Seward 2000-2002
+#
+# This program is covered by the GPL, please refer the to LICENCE file in the
+# distribution; further information can be found in the headers of the scripts
+# in the modules directory.
+###############################################################################
+
+bMotion_plugin_add_complex "sosyour" "you're (a|the) \[^ \]+$" 100 bMotion_plugin_complex_sosyour "en"
+
+proc bMotion_plugin_complex_sosyour {nick host handle channel text} {
+	global botnicks
+
+  if {![bMotion_interbot_me_next $channel]} {
+  	return 1
+  }
+
+	if [regexp -nocase "^($botnicks\[:, \]+)?you're (a|the) (\[a-z\]+)\[.!\]?$" $text matches botnickused bn article noun] {
+    if {$botnickused == ""} {
+			if {[rand 100] < 95} {
+				return 0
+			}
+		}
+		bMotionDoAction $channel $nick "%VAR{sosyours}" "$article $noun"
+		return 1
+  }
+  return 0
+}
+
+bMotion_abstract_register "sosyours"
+bMotion_abstract_batchadd "sosyours" [list "so's your face" "you're %2"]
+
