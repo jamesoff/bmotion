0af281fd022bb41fb341a9a1795bce30f4c25d31
James Seward <james@jamesoff.net>
2005-05-12 21:56:14 +0000

    better regexp better comparison

--- plugins/en/complex_spoonerism.tcl
+++ plugins/en/complex_spoonerism.tcl
@@ -12,12 +12,12 @@
 # in the modules directory.
 ###############################################################################
 
-bMotion_plugin_add_complex "spoon" {^(([^aeiou]+)[aeiuo][a-z]+) ([a-z]+ )?([^aeiou]*([aeiuo][a-z]+))$} 1 bMotion_plugin_complex_spoon "en"
+bMotion_plugin_add_complex "spoon" {^(([^%/aeiou. ]+)[aeiuo][a-z]+) ([a-z]+ )?([^aeiou. ]*([aeiuo][a-z]+))$} 1 bMotion_plugin_complex_spoon "en"
 
 proc bMotion_plugin_complex_spoon { nick host handle channel text } {
 
-  if {[regexp -nocase {^(([^aeiou]+)([aeiuo][a-z]+)) ([a-z]+ )?(([^aeiou]*)([aeiuo][a-z]+))$} $text matches 1 2 3 4 5 6 7]} {
-		if {"$6$3 $4$2$7" != $text} {
+  if {[regexp -nocase {^(([^%/aeiou. ]+)[aeiuo][a-z]+) ([a-z]+ )?([^aeiou. ]*([aeiuo][a-z]+))$} $text matches 1 2 3 4 5 6 7]} {
+		if {![string equal -nocase "$6$3 $4$2$7" $text]} {
     	bMotionDoAction $channel "" "$text ... more like $6$3 $4$2$7, am i rite?"
     	return 1
 		}
