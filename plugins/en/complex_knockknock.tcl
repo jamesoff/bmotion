2177c82a5475f10a0fc8a8092e364bdb671dcb8c
James Seward <james@jamesoff.net>
2005-05-12 23:12:58 +0000

    wheee

--- plugins/en/complex_knockknock.tcl
+++ plugins/en/complex_knockknock.tcl
@@ -12,35 +12,47 @@
 # in the modules directory.
 ###############################################################################
 
-bMotion_plugin_add_complex "knockknock1" {^knock ?knock[!.?]*} 50 bMotion_plugin_complex_knock1 "en"
+bMotion_plugin_add_complex "knockknock1" {^knock ?knock[!.?]*} 80 bMotion_plugin_complex_knock1 "en"
 bMotion_plugin_add_complex "knockknock2" ".+" 100 bMotion_plugin_complex_knock2 "en"
 
 proc bMotion_plugin_complex_knock1 { nick host handle channel text } {
 
 	#TODO: bMotion_interbot_next
 	
-	bMotion_plugin_settings_set "complex:knock" "who" $channel "" $nick
-	bMotion_plugin_settings_set "complex:knock" "state" $channel "" 1
+	bMotion_plugins_settings_set "complex:knock" "who" $channel "" $nick
+	bMotion_plugins_settings_set "complex:knock" "state" $channel "" 1
+	bMotion_plugins_settings_set "complex:knock" "time" $channel "" [clock seconds]
 	bMotionDoAction $channel $nick "%VAR{knock1}"
 	return 1
 }
 
 proc bMotion_plugin_complex_knock2 { nick host handle channel text } {
-	set lastnick [bMotion_plugin_settings_get "complex:knock" "who" $channel ""]
+	set lastnick [bMotion_plugins_settings_get "complex:knock" "who" $channel ""]
+
+	bMotion_flood_undo $nick
+
+	global bMotionOriginalInput
+	set text $bMotionOriginalInput
 
 	if {$nick != $lastnick} {
-		bMotion_plugin_settings_set "complex:knock" "who" $channel "" ""
 		return 0
 	}
 
-	if {[bMotion_plugin_settings_get "complex:knock" "state" $channel ""] == 1} {
+	#check if it's 30s since the start
+	set now [clock seconds]
+	if {$now - [bMotion_plugins_settings_get "complex:knock" "time" $channel ""] > 30} {
+		bMotion_plugins_settings_set "complex:knock" "who" $channel "" ""
+		return 1
+	}
+
+	if {[bMotion_plugins_settings_get "complex:knock" "state" $channel ""] == 1} {
 		#next stage: <answer> who?
-		bMotion_plugin_settings_set "complex:knock" "state" $channel "" 2
+		bMotion_plugins_settings_set "complex:knock" "state" $channel "" 2
 		bMotionDoAction $channel $text "%VAR{knock2}"
 		return 1
 	}
 
-	if {[bMotion_plugin_settings_get "complex:knock" "state" $channel ""] == 2} {
+	if {[bMotion_plugins_settings_get "complex:knock" "state" $channel ""] == 2} {
 		#the end
 		bMotion_plugins_settings_set "complex:knock" "state" $channel "" 0
 		bMotion_plugins_settings_set "complex:knock" "who" $channel "" ""
