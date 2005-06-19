2b2f8b8e50deba7a429250540a12c961dbef94f2
James Seward <james@jamesoff.net>
2005-06-19 15:18:11 +0000

    at long last use an abstract for this

--- plugins/en/complex_spoonerism.tcl
+++ plugins/en/complex_spoonerism.tcl
@@ -18,8 +18,11 @@ proc bMotion_plugin_complex_spoon { nick host handle channel text } {
 
   if {[regexp -nocase {^([^%/aeiou. ]+)([aeiuo][a-z]+) ([a-z]+ )?([^aeiou. ]*)([aeiuo][a-z]+)$} $text matches 1 2 3 4 5 6 7]} {
 		if {![string equal -nocase "$4$2 $3$1$5" $text]} {
-    	bMotionDoAction $channel "" "$text ... more like $4$2 $3$1$5, am i rite?"
+    	bMotionDoAction $channel $text "%VAR{spoonerisms}" "$4$2 $3$1$5"
     	return 1
 		}
   }
 }
+
+bMotion_abstract_register "spoonerisms"
+bMotion_abstract_batchadd "spoonerisms" [list "%% ... more like %2, am i rite?" "%% ... more like %2, am i right?" "%%? More like %2, am I correct?" "/. o O (%2)"]
