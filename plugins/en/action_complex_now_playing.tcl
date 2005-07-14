26ca79e8728601bd2154d7bf059760c3e2f6e418
szrof <no@email.address>
2005-07-14 20:49:29 +0000

    Actually adding new plugins (doh)

---
+++ plugins/en/action_complex_now_playing.tcl
@@ -0,0 +1,26 @@
+## bMotion plugin: hugs
+#
+# $Id$
+#
+
+###############################################################################
+# This is a bMotion plugin
+# Copyright (C) James Michael Seward 2000-2002
+#
+# This program is covered by the GPL, please refer the to LICENCE file in the
+# distribution; further information can be found in the headers of the scripts
+# in the modules directory.
+###############################################################################
+
+bMotion_plugin_add_action_complex "now_playing" "(listening to|now playing|NP:)" 60 bMotion_plugin_complex_action_now_playing "en"
+
+proc bMotion_plugin_complex_action_now_playing { nick host handle channel text } {
+
+  if [bMotionLike $nick $host] {
+    bMotionDoAction $channel $nick "%VAR{nowPlaying}"
+  } else {
+    bMotionDoAction $channel $nick "%VAR{nowPlayingDislike}"
+  }
+  return 1
+}
+
