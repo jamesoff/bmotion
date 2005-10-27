c7f1525d3adf215b1e9036a6804b58808e97aa06
James Seward <james@jamesoff.net>
2005-10-27 15:28:15 +0000

    fixed long-standing error in nazi1 which meant it never worked ever added regexp nazi

--- plugins/en/complex_nazi.tcl
+++ plugins/en/complex_nazi.tcl
@@ -12,7 +12,8 @@
 # in the modules directory.
 ###############################################################################
 
-bMotion_plugin_add_complex "nazi-your" "^your (a |the )?[a-z]+$" 40 bMotion_plugin_complex_nazi1 "en"
+bMotion_plugin_add_complex "nazi-your" "^your (a |the )?\[a-z\]+$" 60 bMotion_plugin_complex_nazi1 "en"
+bMotion_plugin_add_complex "nazi-regexp" "^s/\[^/\]+/\[^/\]+$" 80 bMotion_plugin_complex_nazi2 "en"
 
 proc bMotion_plugin_complex_nazi1 {nick host handle channel text} {
 
@@ -24,6 +25,23 @@ proc bMotion_plugin_complex_nazi1 {nick host handle channel text} {
   return 1
 }
 
+proc bMotion_plugin_complex_nazi2 {nick host handle channel text} {
+	if [string match "\\" $text] {
+		return 0
+	}
+
+	if {![bMotion_interbot_me_next $channel]} {
+		return 1
+	}
+
+	bMotionDoAction $channel $nick "%VAR{nazi2}"
+	return 1
+}
+
 bMotion_abstract_register "nazi1"
 bMotion_abstract_batchadd "nazi1" [list "%%: \"you're\"" "their what?" "s/your/you're/"]
+
+bMotion_abstract_register "nazi2"
+bMotion_abstract_batchadd "nazi2" [list "Invalid regular expression." "You suck at the regular expression syntax." "/detects invalid regexp use" "%%: +/"]
+
 
