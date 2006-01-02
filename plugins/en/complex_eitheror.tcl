51b086b13ea8e34b29a51866c1d0a80cc7c1321d
James Seward <james@jamesoff.net>
2006-01-02 22:42:37 +0000

    new plugin

---
+++ plugins/en/complex_eitheror.tcl
@@ -0,0 +1,35 @@
+## bMotion am not!
+#
+# $Id: complex_fry.tcl 628 2005-08-19 21:30:28Z notopic $
+#
+
+###############################################################################
+# This is a bMotion plugin
+# Copyright (C) James Michael Seward 2000-2002
+#
+# This program is covered by the GPL, please refer the to LICENCE file in the
+# distribution; further information can be found in the headers of the scripts
+# in the modules directory.
+###############################################################################
+
+bMotion_plugin_add_complex "eitheror" {%botnicks[;:,] (.+) or (.+)} 100 bMotion_plugin_complex_eitheror "en"
+
+proc bMotion_plugin_complex_eitheror {nick host handle channel text} {
+	global botnicks
+
+	if [regexp -nocase {([^ ]+) or ([^ ?]+)\?*} $text matches first second] {
+		if [rand 2] {
+			bMotionDoAction $channel $nick "%VAR{eitherors}" $first
+		} else {
+			bMotionDoAction $channel $nick "%VAR{eitherors}" $second
+		}
+		return 1
+	}
+
+	bMotion_flood_undo $nick
+
+  return 0
+}
+
+bMotion_abstract_register "eitherors"
+bMotion_abstract_batchadd "eitherors" [list "%%: %2" "/thinks%|%%: %2" "/ponders%|%%: %2" "/flips a coin%|%%: %2" "%%: %2%|Or you could just use a placebo"]
