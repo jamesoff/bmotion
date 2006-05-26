904284524f2ad7b45a696262b04d75c97462ef0e
James Seward <james@jamesoff.net>
2006-05-26 10:13:50 +0000

    ^_^

---
+++ plugins/en/simple_fish.tcl
@@ -0,0 +1,13 @@
+## bMotion plugin: fish
+#
+
+###############################################################################
+# This is a bMotion plugin
+# Copyright (C) James Michael Seward 2000-2002
+#
+# This program is covered by the GPL, please refer the to LICENCE file in the
+# distribution; further information can be found in the headers of the scripts
+# in the modules directory.
+###############################################################################
+
+bMotion_plugin_add_simple "fish" "^fish!?" 40 [list "today's fish is trout a la creme. enjoy your meal"] "en"
