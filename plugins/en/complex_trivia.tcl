01b4a544fb1190baf41e65fcfcaa20e28bc0f536
James Seward <james@jamesoff.net>
2006-11-01 23:55:45 +0000

    new improved trivia handler

--- plugins/en/complex_trivia.tcl
+++ plugins/en/complex_trivia.tcl
@@ -1,5 +1,6 @@
 ## bMotion plugin: blblbl
 #
+# vim: set foldmarker=<<<,>>>
 # $Id$
 #
 
@@ -13,19 +14,61 @@
 ###############################################################################
 
 #[@   fluffy] ===== Question 1807/12605  =====
-bMotion_plugin_add_complex "trivia1" {== Trivia ==.+\[category: .+\]} 100 bMotion_plugin_complex_trivia_1 "en"
+#bMotion_plugin_add_complex "trivia1" {== Trivia ==.+\[category: .+\]} 100 bMotion_plugin_complex_trivia_1 "en"
 
 #[@   fluffy] Hint: _ _ _ _ _ _ _ _ _
-bMotion_plugin_add_complex "trivia2" {^Hint(:| \[)} 100 bMotion_plugin_complex_trivia_2 "en"
+#bMotion_plugin_add_complex "trivia2" {^Hint(:| \[)} 100 bMotion_plugin_complex_trivia_2 "en"
 
 #[      zeal] Name The Year: Calvin Coolidge, 30th US President, died.
-bMotion_plugin_add_complex "trivia4" {^[\t ]*Name the year:} 100 bMotion_plugin_complex_trivia_4 "en"
+#bMotion_plugin_add_complex "trivia4" {^[\t ]*Name the year:} 100 bMotion_plugin_complex_trivia_4 "en"
 
 # [@   fluffy] Show 'em how it's done Bel! The answer was DARLING.
-bMotion_plugin_add_complex "trivia3" ".+ The (correct )?answer was.+" 100 bMotion_plugin_complex_trivia_3 "en"
+#bMotion_plugin_add_complex "trivia3" ".+ The (correct )?answer was.+" 100 bMotion_plugin_complex_trivia_3 "en"
 
 #
 #bMotion_plugin_add_complex "trivia4" "^Name the year:" 100 bMotion_plugin_complex_trivia_4 "en"
+
+
+proc bMotion_plugin_complex_trivia_botnet { bot command params } {
+	if {$command != "trivia"} {
+		return
+	}
+
+	putlog $params
+
+	if [regexp "^(.+) :(.*)" $params matches cmd arg] {
+		switch $cmd {
+			"start" {
+				bMotion_putloglev d * "trivia: received start command from $bot for channel $arg"
+				bMotion_plugin_complex_trivia_init $arg
+				return
+			}
+
+			"stop" {
+				bMotion_putloglev d * "trivia: received stop command from $bot"
+				bMotion_plugin_complex_trivia_stop
+				return
+			}
+
+			"hint" {
+				bMotion_putloglev d * "trivia: recieved hint from $bot: $arg"
+				bMotion_plugin_complex_trivia_hint $arg
+				return
+			}
+
+			"winner" {
+				bMotion_putloglev d * "trivia: received winner from $bot: $arg"
+				bMotion_plugin_complex_trivia_winner $arg
+				return
+			}
+			default {
+				bMotion_putloglev d * "trivia: received unknown command $cmd with params $arg from $bot"
+			}
+		}
+	}
+}
+
+bind bot - "trivia" bMotion_plugin_complex_trivia_botnet
 
 #
 # Detect the start of the game
@@ -38,6 +81,93 @@ proc bMotion_plugin_complex_trivia_1 { nick host handle channel text } {
 	return 2
 }
 
+proc bMotion_plugin_complex_trivia_delay { } {
+	set delay [bMotion_plugins_settings_get "trivia" "delay" "" ""]
+	if {$delay != ""} {
+		return [expr [rand [expr $delay - 3]] + 3]
+	}
+}
+
+# be told about the start of a round remotely
+proc bMotion_plugin_complex_trivia_init { info } {
+	if [regexp {(#[^ ]+) ([0-9]+)} $info matches channel roundlength] {
+		bMotion_plugins_settings_set "trivia" "channel" "" "" $channel
+		bMotion_plugins_settings_set "trivia" "type" "" "" "normal"
+		bMotion_plugins_settings_set "trivia" "played" "" "" 0
+		bMotion_plugins_settings_set "trivia" "hint" "" "" ""
+		bMotion_plugins_settings_set "trivia" "tries" "" "" 0
+		bMotion_plugins_settings_set "trivia" "delay" "" "" $roundlength
+		bMotion_putloglev 1 * "trivia: informed of round start in $channel, delay = $roundlength"
+
+		#start a short timer (add extra delay for the text to escape from TriviaCow)
+		set delay [expr [bMotion_plugin_complex_trivia_delay] + 4]
+		bMotion_putloglev 1 * "trivia: try guessing in $delay seconds"
+		bMotion_plugins_settings_set "trivia" "timer" "" "" [utimer $delay bMotion_plugin_complex_trivia_auto]
+	}
+}
+
+# be told about a new hint
+proc bMotion_plugin_complex_trivia_hint { hint } {
+	bMotion_plugins_settings_set "trivia" "hint" "" "" $hint
+	bMotion_putloglev 1 * "trivia: informed of hint: $hint"
+}
+
+# be told the round is over and who won
+proc bMotion_plugin_complex_trivia_winner { info } {
+	bMotion_plugins_settings_set "trivia" "type" "" "" ""
+	bMotion_plugins_settings_set "trivia" "hint" "" "" ""
+	set channel [bMotion_plugins_settings_get "trivia" "channel" "" ""]
+
+	if [regexp {^([^ ]+) (.+)} $info matches winner answer] {
+		set answer [string tolower $answer]
+		bMotion_putloglev d * "trivia: answer was $answer"
+
+		set words [split $answer " "]
+		foreach word $words {
+			if [regexp {([a-zA-Z1]+)} $word matches newword] {
+				set word $newword
+			}
+			set word [string tolower $word]
+			set firstletter [string toupper [string range $word 0 0]]
+			set full_array_name_for_upvar "afro_$firstletter"
+			bMotion_putloglev d * "trivia: adding $word to $full_array_name_for_upvar"
+			bMotion_abstract_add $full_array_name_for_upvar $word
+		}
+
+		#react to who won
+		bMotion_plugins_settings_set "trivia" "channel" "" "" ""
+
+		if {$winner == "*"} {
+			#noone won
+			bMotion_putloglev d * "trivia: noone won this round"
+      if {![rand 20]} {
+        bMotionDoAction $channel $nick "%VAR{bahs}"
+      }
+		} else {
+			# someone got it right
+			if [isbotnick $winner] {
+				#it was me
+				bMotion_putloglev d * "trivia: i won!"
+				bMotionDoAction $channel "" "%VAR{trivia_wins}"
+			} else {
+				#it was someone else
+				bMotion_putloglev d * "trivia: somone else won :("
+        if {(![rand 10]) && ([bMotion_plugins_settings_get "trivia" "type" "" ""] != "year")} {
+          bMotionDoAction $channel $nick "%VAR{trivia_loses}"
+        }
+			}
+		}
+	}
+}
+
+# be told to stop (i.e. someone stopped the game manually
+proc bMotion_plugin_complex_trivia_stop { } {
+	bMotion_plugins_settings_set "trivia" "type" "" "" ""
+	bMotion_plugins_settings_set "trivia" "channel" "" "" ""
+	bMotion_plugins_settings_set "trivia" "hint" "" "" ""
+}
+
+# name the year question
 proc bMotion_plugin_complex_trivia_4 { nick host handle channel text } {
   if {$channel != [bMotion_plugins_settings_get "trivia" "channel" "" ""]} {
     return 0
@@ -78,7 +208,7 @@ proc bMotion_plugin_complex_trivia_2 { nick host handle channel text } {
   #bMotion_plugin_complex_trivia_guess $nick $host $handle $channel $text
 
   #instead start a short timer
-  set delay [expr [rand 15] + 2]
+  set delay [bMotion_plugin_complex_trivia_delay]
   bMotion_putloglev d * "try trivia first again in $delay seconds"
   bMotion_plugins_settings_set "trivia" "timer" "" "" [utimer $delay bMotion_plugin_complex_trivia_auto]
 	return 2
@@ -144,34 +274,45 @@ proc bMotion_plugin_complex_trivia_guess { nick host handle channel text } {
   set bMotionOriginalInput $text
 
   set tries [bMotion_plugins_settings_get "trivia" "tries" "" ""]
+	if {$tries == ""} {
+		set tries 0
+	}
   incr tries
   if {$tries > 4} {
     #give up auto-guessing until next clue
-    bMotion_putloglev d * "giving up guessing trivia after 4 tries..."
+    bMotion_putloglev 1 * "trivia: giving up guessing trivia after 4 tries..."
     return 0
   }
   bMotion_plugins_settings_set "trivia" "tries" "" "" $tries
+	bMotion_putloglev 1 * "trivia: making attempt $tries"
 
   #remove {}s
-  regsub -all {[\{\}]} $text " " text
+  #regsub -all {[\{\}]} $text " " text
 
   #extract the hint
-  regexp {^Hint(:| \[.+ of .+\]:) ([_ A-Z])+} $text matches hinttext
-  catch {
-    if {$hinttext == ""} {
-      return 2
-    }
-  }
-  set hinttext [string range $text 6 end]
-  set text [string trim $text]
+  #regexp {^Hint(:| \[.+ of .+\]:) ([_ A-Z])+} $text matches hinttext
+  #catch {
+    #if {$hinttext == ""} {
+     # return 2
+    #}
+  #}
+  #set hinttext [string range $text 6 end]
+  #set text [string trim $text]
 
   #split if needed
-  regsub -all { ([_A-Z])} $hinttext "\\1" hinttext
+  #regsub -all { ([_A-Z])} $hinttext "\\1" hinttext
 
   #turn _s to .s to make a regexp
-  regsub -all "_" $hinttext "." hinttext
+  #regsub -all "_" $hinttext "." hinttext
 
-  bMotion_putloglev 2 * "contracted hint is $hinttext"
+  #bMotion_putloglev 2 * "contracted hint is $hinttext"
+	set hinttext [bMotion_plugins_settings_get "trivia" "hint" "" ""]
+	if {$hinttext == ""} {
+		bMotion_putloglev d * "trivia: no hint, aborting guess"
+		return
+	}
+
+	bMotion_putloglev 1 * "trivia: current hint is $hinttext"
 
   set hintlist [split $hinttext " "]
   set answer ""
@@ -181,17 +322,19 @@ proc bMotion_plugin_complex_trivia_guess { nick host handle channel text } {
   foreach hint $hintlist {
     set hint [string trim $hint]
     if {$hint != ""} {
-      bMotion_putloglev 2 * "processing hint $hint"
+      bMotion_putloglev 1 * "trivia: processing hint $hint"
       set firstletter [string range $hint 0 0]
       if {$firstletter == "."} {
+				bMotion_putloglev 1 * "trivia: unknown first char, making it up"
         if {[bMotion_plugins_settings_get "trivia" "type" "" ""] == "year"} {
           set firstletter "1"
         } else {
           set firstletter [pickRandom [split "ABCDEFGHIJKLMNOPQRSTUVWXYZ" {}]]
+					bMotion_putloglev 1 * "trivia: invented $firstletter as first letter"
         }
       }
       if [regexp {[A-Z1]} $firstletter] {
-        bMotion_putloglev 1 * "looking for a $firstletter word..."
+        bMotion_putloglev 1 * "trivia: looking for a $firstletter word..."
         set upvar_name "afro_$firstletter"
         set wordlist [bMotion_abstract_all $upvar_name]
         #find a matching word
@@ -219,29 +362,38 @@ proc bMotion_plugin_complex_trivia_guess { nick host handle channel text } {
       #putlog "about to call bMotionDoAction"
       #bMotionDoAction $channel $nick $answer lalala 1
       bMotionDoAction $channel $nick $answer nothing 1
-      putloglev d * "answered trivia with $answer"
+      bMotion_putloglev d * "trivia: answered trivia with $answer"
       bMotion_plugins_settings_set "trivia" "last" "" "" $answer
       bMotion_plugins_settings_set "trivia" "played" "" "" 1
 
       #since we have an answer and it's different, let's have another guess shortly
       bMotion_plugins_settings_set "trivia" "hint" "" "" $bMotionOriginalInput
       bMotion_plugins_settings_set "trivia" "channel" "" "" $channel
-      set delay [expr [rand 15] + 10]
-      bMotion_putloglev d * "will try trivia again in $delay seconds"
+      set delay [bMotion_plugin_complex_trivia_delay]
+      bMotion_putloglev d * "trivia: will try trivia again in $delay seconds"
       bMotion_plugins_settings_set "trivia" "timer" "" "" [utimer $delay bMotion_plugin_complex_trivia_auto]
     } else {
-      putloglev d * "skipped answering with $answer, same as last time"
+      bMotion_putloglev d * "trivia: skipped answering with $answer, same as last time"
     }
-  }
+  } else {
+		bMotion_putloglev d * "trivia: unable to find enough matching words, not guessing"
+		if [regexp {^[. ]+$} [bMotion_plugins_settings_get "trivia" "hint" "" ""]] {
+			set delay [bMotion_plugin_complex_trivia_delay]
+			bMotion_putloglev d * "trivia: will try trivia again in $delay seconds"
+			bMotion_plugins_settings_set "trivia" "timer" "" "" [utimer $delay bMotion_plugin_complex_trivia_auto]
+		}
+	}
 	return 2
 }
 
 proc bMotion_plugin_complex_trivia_auto { } {
-  set channel [bMotion_plugins_settings_get "trivia" "channel" "" ""]
-  if {$channel != ""} {
-    bMotion_putloglev d * "auto-guessing for trivia again..."
-    bMotion_plugin_complex_trivia_guess "" "" "" $channel [bMotion_plugins_settings_get "trivia" "hint" "" ""]
-  }
+	if {[bMotion_plugins_settings_get "trivia" "type" "" ""] != ""} {
+		set channel [bMotion_plugins_settings_get "trivia" "channel" "" ""]
+		if {$channel != ""} {
+			bMotion_putloglev d * "auto-guessing for trivia again..."
+			bMotion_plugin_complex_trivia_guess "" "" "" $channel [bMotion_plugins_settings_get "trivia" "hint" "" ""]
+		}
+	}
 }
 
 #set up the abstracts
