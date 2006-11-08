08a99074b242396d044f3c9ed2ef8c5c5b44be7b
James Seward <james@jamesoff.net>
2006-11-08 17:06:14 +0000

    new plugin

---
+++ plugins/en/complex_plus.tcl
@@ -0,0 +1,21 @@
+## bMotion plugin: plus
+#
+# vim: fdm=indent fdn=1
+
+###############################################################################
+# This is a bMotion plugin
+# Copyright (C) James Michael Seward 2000-2002
+#
+# This program is covered by the GPL, please refer the to LICENCE file in the
+# distribution; further information can be found in the headers of the scripts
+# in the modules directory.
+###############################################################################
+
+bMotion_plugin_add_complex "plus" {^[a-z]+ plus$} 75 bMotion_plugin_complex_plus "en"
+
+proc bMotion_plugin_complex_plus { nick host handle channel text } {
+	if [regexp -nocase {^([a-z]+) plus$} $text matches item] {
+		bMotionDoAction $channel $item "like regular %% but with a cartridge slot"
+		return 1
+	}
+}
