8f3791b9f6bc00970dfd8e60c26267adfe241886
James Seward <james@jamesoff.net>
2007-01-02 20:10:00 +0000

    fix #112 remove stray broken line from end of irc_join.tcl

--- plugins/en/irc_join.tcl
+++ plugins/en/irc_join.tcl
@@ -1,6 +1,5 @@
 # $Id$
 #
-# vim: fdm=indent fdn=1
 
 ###############################################################################
 # This is a bMotion plugin
@@ -14,63 +13,63 @@
 
 proc bMotion_plugins_irc_default_join { nick host handle channel text } { 
 
-  #has something happened since we last greeted?
-  set lasttalk [bMotion_plugins_settings_get "system:join" "lasttalk" $channel ""]
+	#has something happened since we last greeted?
+	set lasttalk [bMotion_plugins_settings_get "system:join" "lasttalk" $channel ""]
 
-  #if 1, we greeted someone last
-  #if 0, someone has said something since
-  if {$lasttalk == 1} {
-    bMotion_putloglev 2 d "dropping greeting for $nick on $channel because it's too idle"
-    return 0
-  }
+	#if 1, we greeted someone last
+	#if 0, someone has said something since
+	if {$lasttalk == 1} {
+		bMotion_putloglev 2 d "dropping greeting for $nick on $channel because it's too idle"
+		return 0
+	}
 
 	#we must also see if we're next to greet
 	if {![bMotion_interbot_me_next $channel]} {
 		return 1
 	}
 	
-  global botnick mood
-  set chance [rand 10]
+	global botnick mood
+	set chance [rand 10]
 
-  set greetings [bMotion_abstract_all "ranjoins"]
+	set greetings [bMotion_abstract_all "ranjoins"]
 	set lastLeft [bMotion_plugins_settings_get "system:join" "lastleft" $channel ""]
 
-	if {[bMotion_settings_get "friendly"] == "2"} {
+	if {[bMotion_setting_get "friendly"] == "2"} {
 		# don't greet anyone
 		return 0
 	}
 
 	if {$handle != "*"} {
-    if {![rand 10]} {
-      set greetings [concat $greetings [bMotion_abstract_all "insult_joins"]]
-    }
-    if {$nick == $lastLeft} {
-      set greetings [bMotion_abstract_all "welcomeBacks"]
-      bMotion_plugins_settings_set "system:join" "lastleft" $channel "" ""
-    }
-    bMotionGetHappy
-    bMotionGetUnLonely
-  } else {
-    #don't greet people we don't know
-    if {[bMotion_setting_get "friendly"] != 1} {
-      return 0
-    }
-  }
+		if {![rand 10]} {
+			set greetings [concat $greetings [bMotion_abstract_all "insult_joins"]]
+		}
+		if {$nick == $lastLeft} {
+			set greetings [bMotion_abstract_all "welcomeBacks"]
+			bMotion_plugins_settings_set "system:join" "lastleft" $channel "" ""
+		}
+		bMotionGetHappy
+		bMotionGetUnLonely
+	} else {
+		#don't greet people we don't know
+		if {[bMotion_setting_get "friendly"] != 1} {
+			return 0
+		}
+	}
 
-  #set nick [bMotion_cleanNick $nick $handle]
-  if {[getFriendship $nick] < 30} {
-    set greetings [bMotion_abstract_all "dislike_joins"]
-  }
+	#set nick [bMotion_cleanNick $nick $handle]
+	if {[getFriendship $nick] < 30} {
+		set greetings [bMotion_abstract_all "dislike_joins"]
+	}
 
-  if {[getFriendship $nick] > 75} {
-    set greetings [concat $greetings [bMotion_abstract_all "bigranjoins"]]
-  }
+	if {[getFriendship $nick] > 75} {
+		set greetings [concat $greetings [bMotion_abstract_all "bigranjoins"]]
+	}
 
-  bMotionDoAction $channel [bMotionGetRealName $nick $host] [pickRandom $greetings]
-  bMotion_plugins_settings_set "system:join" "lastgreeted" $channel "" $nick
-  bMotion_plugins_settings_set "system:join" "lasttalk" $channel "" 1
+	bMotionDoAction $channel [bMotionGetRealName $nick $host] [pickRandom $greetings]
+	bMotion_plugins_settings_set "system:join" "lastgreeted" $channel "" $nick
+	bMotion_plugins_settings_set "system:join" "lasttalk" $channel "" 1
 
-  return 1
+	return 1
 }
 
 bMotion_plugin_add_irc_event "default join" "join" ".*" 40 "bMotion_plugins_irc_default_join" "en"
@@ -83,4 +82,3 @@ bMotion_abstract_batchadd "wrong_infoline" [list "oops, wrong infoline, sorry" "
 
 bMotion_abstract_register "dislike_joins"
 bMotion_abstract_batchadd "dislike_joins" [list "shut up" "o no it's %%" "oh no it's %%" "oh noes it's %% %VAR{unsmiles}"]
-bMot
--- plugins/en/irc_quit.tcl
+++ plugins/en/irc_quit.tcl
@@ -1,6 +1,5 @@
 # $Id$
 #
-# vim: fdm=indent fdn=1
 
 ###############################################################################
 # This is a bMotion plugin
@@ -14,45 +13,45 @@
 
 proc bMotion_plugins_irc_default_quit { nick host handle channel text } { 
 
-  #has something happened since we last spoke?
-  set lasttalk [bMotion_plugins_settings_get "system:join" "lasttalk" $channel ""]
+	#has something happened since we last spoke?
+	set lasttalk [bMotion_plugins_settings_get "system:join" "lasttalk" $channel ""]
 
-	if {[bMotion_settings_get "friendly"] == "2"} {
+	if {[bMotion_setting_get "friendly"] == "2"} {
 		return 0
 	}
 
-  if {$handle == "*"} {
-    if {[bMotion_setting_get "friendly"] != 1} {
-      return 0
-    }
-  }
-  
-  if {[bMotionIsFriend $nick]} {
-  	set output "%VAR{departs-nice}"
-  } else {
-  	set output "%VAR{departs-nasty}"
-  }
-  
+	if {$handle == "*"} {
+		if {[bMotion_setting_get "friendly"] != 1} {
+			return 0
+		}
+	}
+	
+	if {[bMotionIsFriend $nick]} {
+		set output "%VAR{departs-nice}"
+	} else {
+		set output "%VAR{departs-nasty}"
+	}
+	
 
-  #don't do anything if it looks like an error
-  if [regexp -nocase "(irc\.|error|reset|timeout|closed)" $text] {
-    return 0
-  }
+	#don't do anything if it looks like an error
+	if [regexp -nocase "(irc\.|error|reset|timeout|closed)" $text] {
+		return 0
+	}
 
-  #if 1, we greeted someone last
-  #if 0, someone has said something since
-  if {$lasttalk == 1} {
-    #bMotion_putloglev 2 d "dropping depart for $nick on $channel because it's too idle"
-    bMotion_putloglev d * "dropping depart for $nick on $channel because it's too idle"
-    return 0
-  }
+	#if 1, we greeted someone last
+	#if 0, someone has said something since
+	if {$lasttalk == 1} {
+		#bMotion_putloglev 2 d "dropping depart for $nick on $channel because it's too idle"
+		bMotion_putloglev d * "dropping depart for $nick on $channel because it's too idle"
+		return 0
+	}
 
-  bMotionDoAction $channel [bMotionGetRealName $nick $host] $output
-  bMotion_plugins_settings_set "system:join" "lasttalk" $channel "" 1
+	bMotionDoAction $channel [bMotionGetRealName $nick $host] $output
+	bMotion_plugins_settings_set "system:join" "lasttalk" $channel "" 1
 	bMotion_plugins_settings_set "system:join" "lastleft" $channel "" $nick
 	bMotion_plugins_settings_set "system:join" "lastgreeted" $channel "" $nick
 
-  return 1
+	return 1
 }
 
 bMotion_plugin_add_irc_event "default quit" "quit" ".*" 15 "bMotion_plugins_irc_default_quit" "en"
