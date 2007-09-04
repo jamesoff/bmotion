bb3111ca3c8b0213ab6a72667577af9b6bda1589
James Seward <james@jamesoff.net>
2007-09-04 12:16:12 +0000

    improve trigger for this plugin

--- plugins/en/complex_readings.tcl
+++ plugins/en/complex_readings.tcl
@@ -13,11 +13,11 @@
 # in the modules directory.
 ###############################################################################
 
-bMotion_plugin_add_complex "st-readings" "%botnicks:?,? activate (.+) (detectors?|sensors?)!?$" 100 bMotion_plugin_complex_readings "en"
+bMotion_plugin_add_complex "st-readings" "%botnicks:?,? (enable|activate) (.+) (detectors?|sensors?)!?$" 100 bMotion_plugin_complex_readings "en"
 
 proc bMotion_plugin_complex_readings { nick host handle channel text } {
   global botnicks
-  if [regexp -nocase "${botnicks}:?,? activate (.+) (detectors?|sensors?)!?$" $text ming bhar reading] {
+  if [regexp -nocase "${botnicks}:?,? (enable|activate) (.+) (detectors?|sensors?)!?$" $text ming bhar moo reading] {
     set readingLevel [rand 101]
     bMotionDoAction $channel $channel "%VAR{readings_scan}"
     set reading "[string toupper [string range $reading 0 0]][string range $reading 1 end]"
