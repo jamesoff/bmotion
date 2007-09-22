f02ef2107f2729e6be5d0c2ec9523b514f94219a
Mark Sangster <no@email.address>
2007-09-22 11:54:28 +0000

    Snap plugin only responses if within 10 minutes, fixing #145

--- plugins/en/complex_snap.tcl
+++ plugins/en/complex_snap.tcl
@@ -1,8 +1,8 @@
-## bMotion plugin: blblbl
+## bMotion plugin: snap
 #
 # $Id$
 #
-# vim: fdm=indent fdn=1
+# vim: fdm=indent fdn=1 sts=4 ts=8 et
 
 ###############################################################################
 # This is a bMotion plugin
@@ -16,23 +16,30 @@
 bMotion_plugin_add_complex "snap" "." 100 bMotion_plugin_complex_snap "en"
 
 proc bMotion_plugin_complex_snap { nick host handle channel text } {
-  if {[string length $text] < 5} {
-    return 0
-  }
-  if {($text == [bMotion_plugins_settings_get "complex:snap" $channel "" "text"]) &&
-      ($nick != [bMotion_plugins_settings_get "complex:snap" $channel "" "nick"])} {
+    if {[string length $text] < 5} {
+        return 0
+    }
+
+    set ctime [clock seconds]
+    
+    if {
+        ($text == [bMotion_plugins_settings_get "complex:snap" $channel "" "text"]) &&
+        ($nick != [bMotion_plugins_settings_get "complex:snap" $channel "" "nick"]) &&
+        ($ctime - [bMotion_plugins_settings_get "complex:snap" $channel "" "time"] < 600)
+    } {
         if {[bMotion_interbot_me_next $channel] && [rand 2]} {
-          set othernick [bMotion_plugins_settings_get "complex:snap" $channel "" "nick"]
-          bMotionDoAction $channel $nick "%VAR{snaps}" $othernick
-          bMotion_plugins_settings_set "complex:snap" $channel "" "text" ""
-          bMotion_plugins_settings_set "complex:snap" $channel "" "nick" ""
-					return 1
+            set othernick [bMotion_plugins_settings_get "complex:snap" $channel "" "nick"]
+            bMotionDoAction $channel $nick "%VAR{snaps}" $othernick
+            bMotion_plugins_settings_set "complex:snap" $channel "" "text" ""
+            bMotion_plugins_settings_set "complex:snap" $channel "" "nick" ""
+            bMotion_plugins_settings_set "complex:snap" $channel "" "time" 0
+            return 1
         }
-      }
-  bMotion_plugins_settings_set "complex:snap" $channel "" "text" $text
-  bMotion_plugins_settings_set "complex:snap" $channel "" "nick" $nick
-  return 0
+    }
+    bMotion_plugins_settings_set "complex:snap" $channel "" "text" $text
+    bMotion_plugins_settings_set "complex:snap" $channel "" "nick" $nick
+    bMotion_plugins_settings_set "complex:snap" $channel "" "time" $ctime
+    return 0
 }
 
-bMotion_abstract_register "snaps"
-bMotion_abstract_batchadd "snaps" [list "o snap" "SNAP" "SNAP!" "/wins %% and %2"]
+bMotion_abstract_register "snaps" [list "o snap" "SNAP" "SNAP!" "/wins %% and %2"]
