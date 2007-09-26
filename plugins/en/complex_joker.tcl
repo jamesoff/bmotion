cd56fed79b8f9a5153f6d89bfabe8ac7396fda47
James Seward <james@jamesoff.net>
2007-09-26 22:31:49 +0000

    fix!

--- plugins/en/complex_joker.tcl
+++ plugins/en/complex_joker.tcl
@@ -13,37 +13,75 @@
 # in the modules directory.
 ###############################################################################
 
-# tell a random joke plugin
-set jokeInfo ""
-
 # register the invoke a joke callback
 bMotion_plugin_add_complex "joker" "^(%botnicks:? )?!joke" 100 "bMotion_plugin_complex_invoke_joke" "en"
 
-# tell a random answer callback
-proc bMotionDoJokeAnswer {} {
-	global jokeInfo bMotionFacts
+# random joke callback
+proc bMotion_plugin_complex_invoke_joke { nick host handle channel text } {
+	global bMotion_abstract_contents jokeInfo 
 
-	# if we're not telling a joke, what are we doing here?
-	if { $jokeInfo == "" } { return 0 }
-
-	set jokeReplies [bMotion_abstract_all "jokeReplies"]
-	putlog $jokeReplies
-	
-	# parse out the first 3 bits of info
-	regexp -nocase "(.+)¦(.+)¦(.+):(.+)" $jokeInfo pop nick channel index bits
-	
-	# coordinate with the joke
-	set answer [ lindex $jokeReplies $index ]
-	
-	# parse relational nouns 
-	while { [string first "%r" $answer] != -1 } {
-		set ind [string first "¦" $bits]
-		if { $ind == -1 } {
-			bMotionDoAction $channel $nick "%VAR{lostPlot}"
-			return 0
+	# I'll have no part in this if i'm telling a joke already
+	set timestamp [bMotion_plugins_settings_get "complex:joker" $channel "" ""]
+	if {$timestamp != ""} {
+		set now [clock seconds]
+		if {[expr $now - $timestamp] < 10} {
+			bMotion_putloglev d * "being asked to tell a joke too soon after the last one, ignoring"
+			# but we want to stop people trying to flood us
+			return 1
 		}
-		set thing [string range $bits 0 [ expr $ind - 1 ] ]
-		set bits [string range $bits [ expr $ind + 2 ] end ]
+	}
+
+	if { ![bMotion_interbot_me_next $channel] } {
+		return 0
+	}
+
+	bMotion_plugins_settings_set "complex:joker" $channel "" "" [clock seconds]
+
+	# we need the index to coordinate with the reply
+	# %n is random noun
+	set jokeForms {
+		"what's the difference between %n and %n?"
+		"what do you get when you cross %n and %n?"
+		"what do you think you could do with %n?"
+		"how do you make %n?"
+		"did you hear the one about %n?"
+		"what's %VAR{colours} and invisible?"
+		"what's %VAR{colours} and sticky?"
+	}
+
+	# %r is relational
+	# %n is random noun
+	set jokeReplies {
+		"one's %r and the other's %r"
+		"%r with %r!"
+		"become %r"
+		"use %r"
+		"it was %r"
+		"no %PLURAL{%VAR{sillyThings}{strip}}"
+		"%n"
+	}
+
+	set index [ rand [ llength $jokeForms ] ]
+	set joke [ lindex $jokeForms $index ]
+
+	# have a different random thing for every %n tag
+	# this is here because the abstract replaces them all with the same silly thing
+	# instead of one for each occurance. That might be expected behaviour, so we'll
+	# just do it here instead.
+	set things [list]
+	while { [string first "%n" $joke] != -1 } {
+		set object [bMotion_abstract_get "sillyThings"]
+		regsub "%n" $joke $object joke
+		lappend things $object
+	}
+	lappend things ""
+
+
+	# prepare the answer
+	set answer [ lindex $jokeReplies $index ]
+
+	while { [string first "%r" $answer] != -1 } {
+		set thing [lindex things 0]
 		set object ""
 		catch {
 			set answers $bMotionFacts(what, $thing)
@@ -56,91 +94,19 @@ proc bMotionDoJokeAnswer {} {
 		}
 		
 		regsub "%r" $answer $object answer
+		set things [lreplace $things 0 1]
 	}
 
-	# parse non-relational
 	while { [string first "%n" $answer] != -1 } {
 		set object [bMotion_abstract_get "sillyThings"]
 		regsub "%n" $answer $object answer
 	}
 
-	# tell the answer
-	bMotionDoAction $channel $nick "%DELAY{15}%|$answer"
-	
-	# reset joke
-	set jokeInfo ""
-
-	# log this
-	bMotion_putloglev d * "bMotion: (joker) I made a funny"
-	return 1 
-}
-
-# random joke callback
-proc bMotion_plugin_complex_invoke_joke { nick host handle channel text } {
-	global bMotion_abstract_contents jokeInfo 
-
-	if { ![bMotion_interbot_me_next $channel] } {
-		return 0
-	}
-
-	# check if we're already telling a joke
-	if { $jokeInfo != "" } {
-		bMotionDoAction $channel $nick "I'm sorry, but can't you see I'm already telling a joke?!"
-		return 1
-	}
-
-	# we need the index to coordinate with the reply
-	set jokeForms [bMotion_abstract_all "jokeForms"]
-	set index [ rand [ llength $jokeForms ] ]
-	set joke [ lindex $jokeForms $index ]
-	# set the startings of the joke info
-	set jokeInfo "$nick¦$channel¦$index:"
-
-	# have a different random thing for every %n tag
-	# this is here because the abstract replaces them all with the same silly thing
-	# instead of one for each occurance. That might be expected behaviour, so we'll
-	# just do it here instead.
-	while { [string first "%n" $joke] != -1 } {
-		set object [bMotion_abstract_get "sillyThings"]
-		regsub "%n" $joke $object joke
-		set jokeInfo "$jokeInfo$object¦"
-	}
-
 	# tell the joke
 	bMotionDoAction $channel $nick $joke
-	
-	# pause before telling the answer
-	#utimer 20 bMotionDoJokeAnswer
-	#now we use %DELAY, this can just happen
-	bMotionDoJokeAnswer
-	
-	# log this
-	bMotion_putloglev d * "bMotion: (joker) I'm a tellin' a joke"
-
-	# clear this now we're not calling the support function later
-	set jokeInfo ""
+	bMotionDoAction $channel $nick "%DELAY{10}%|$answer"
 
 	return 1 
 }
 
-# %n is random noun
-bMotion_abstract_register "jokeForms"
-bMotion_abstract_batchadd "jokeForms" {
-  "what's the difference between %n and %n?"
-  "what do you get when you cross %n and %n?"
-  "what do you think you could do with %n?"
-  "how do you make %n?"
-  "did you hear the one about %n?"
-}
-
-# %r is relational
-# %n is random noun
-bMotion_abstract_register "jokeReplies"
-bMotion_abstract_batchadd "jokeReplies" {
-  "one's %r and the other's %r"
-  "%r with %r!"
-  "become %r"
-  "use %r"
-  "it was %r"
-}
 
