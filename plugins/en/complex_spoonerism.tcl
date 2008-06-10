10ff56f365e337acea86c19e3eda4ddc21281ffe
James Seward <james@jamesoff.net>
2008-06-10 10:54:56 +0000

    fix

--- plugins/en/complex_spoonerism.tcl
+++ plugins/en/complex_spoonerism.tcl
@@ -37,7 +37,7 @@ proc bMotion_plugin_complex_spoon2 { nick host handle channel text } {
 		return 0
 	}
 
-	if [regexp -nocase {([b-df-hj-np-tv-xz]+)([aeiou]{2})([b-df-hj-np-tv-xz]+[.!?]?)} $text matches 1 2 3 4] {
+	if [regexp -nocase {([b-df-hj-np-tv-xz]+)(([aeiou])\3)([b-df-hj-np-tv-xz]+[.!?]?)} $text matches 1 2 3 4] {
 		bMotionDoAction $channel $text "$1%REPEAT{2:10:$2}$3$4"
 		return 1
 	}
