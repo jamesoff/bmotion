3c082c531d088812a000dce79cf10e36da260f9f
James Seward <james@jamesoff.net>
2009-04-22 18:00:16 +0000

    fix broken debug output fix not greeting unknown users due to multiple handles matching '*'

--- plugins/en/irc_join.tcl
+++ plugins/en/irc_join.tcl
@@ -13,7 +13,7 @@
 proc bMotion_plugins_irc_default_join { nick host handle channel text } { 
   #if the user isban then dont bother
   if {[ischanban $nick $channel] == 1 } {
-    bMotion_putloglev 2 d "dropping greeting for $nick on $channel as user is banned"
+    bMotion_putloglev d * "dropping greeting for $nick on $channel as user is banned"
     return 0
   }
 
@@ -23,32 +23,37 @@ proc bMotion_plugins_irc_default_join { nick host handle channel text } {
 	#if 1, we greeted someone last
 	#if 0, someone has said something since
 	if {$lasttalk == 1} {
-		bMotion_putloglev 2 d "dropping greeting for $nick on $channel because it's too idle"
+		bMotion_putloglev d * "dropping greeting for $nick on $channel because it's too idle"
 		return 0
 	}
 
-	# check if the user is already in the channel - probably means they've joined a 2nd client
-	# or that they're about to ping out or something
-	set count 0
-	foreach n [chanlist $channel] {
-		if {[nick2hand $n $channel] == $handle} {
+	# skip this check if handle is * as it's pointless
+	if {$handle == "*"} {
+		# check if the user is already in the channel - probably means they've joined a 2nd client
+		# or that they're about to ping out or something
+		set count 0
+		foreach n [chanlist $channel] {
+			if {[nick2hand $n $channel] == $handle} {
 			# we have a match
-			incr count
+				incr count
+			}
 		}
-	}
-	
-	if {$count >= 2} {
-		bMotion_putloglev d * "$nick has $count matching handles in $channel, so not greeting"
-		return 1
+
+		if {$count >= 2} {
+			bMotion_putloglev d * "$nick has $count matching handles in $channel, so not greeting"
+			return 1
+		}
 	}
 
 	#we must also see if we're next to greet
 	if {![bMotion_interbot_me_next $channel]} {
+		bMotion_putloglev 1 * "we're not next for $channel, so not greeting"
 		return 1
 	}
 	
 	if {[bMotion_setting_get "friendly"] == "2"} {
 		# don't greet anyone
+		bMotion_putloglev 1 * "we're friendly==2 so not greeting $nick in $channel"
 		return 0
 	}
 
@@ -59,6 +64,7 @@ proc bMotion_plugins_irc_default_join { nick host handle channel text } {
 
 	if {$handle == "*"} {
 		if {[bMotion_setting_get "friendly"] != "1"} {
+			bMotion_putloglev d * "$nick has no handle and friendly!=1 so not greeting"
 			return 0
 		}
 		set greetings "unknown_joins"
@@ -70,6 +76,7 @@ proc bMotion_plugins_irc_default_join { nick host handle channel text } {
 	if {$handle != "*"} {
 		if {![rand 10]} {
 			set greetings "insult_joins"
+			bMotion_putloglev d * "decided to be rude to $nick on join as i know them"
 		}
 		bMotionGetHappy
 		bMotionGetUnLonely
