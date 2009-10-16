5a35045e1f5fbd80095de77df4043a8b34bf5680
James Seward <james@jamesoff.net>
2009-10-16 19:23:28 +0000

    did... something

--- plugins/en/complex_sucks.tcl
+++ plugins/en/complex_sucks.tcl
@@ -14,24 +14,48 @@
 bMotion_plugin_add_complex "sucks" {sucks?$} 30 bMotion_plugin_complex_sucks "en"
 
 proc bMotion_plugin_complex_sucks { nick host handle channel text } {
-		global botnicks
+	global botnicks
 
-		if [regexp -nocase {([^ ]+) ((is|si|==) (teh|the)? )?sucks?} $text matches item] {
-				if {$item == "=="} {
-						return 0
-				}
+	if [regexp -nocase {([a-z0-9]+) (all )?suck[.!]*$} $text matches item] {
+		bMotionDoAction $channel $item "%VAR{sucks}"
+		return 1
+	}
 
-				if {[string tolower $item] == "i"} {
-						return 0
-				}
-
-				if [regexp -nocase "you|we|really|=|i|$botnicks" $item] {
-						return 0
-				}
-
-				bMotionDoAction $channel $item "%VAR{sucks}"
-				return 1
+	if [regexp -nocase {^([a-z0-9]+) sucks} $text matches item] {
+		if [regexp -nocase "^(that|you|which|that|it|what)$" $item] {
+			return 0
 		}
+		bMotionDoAction $channel $item "%VAR{sucks}"
+		return 1
+	}
+
+	return 0
+
+	# DEAD CODE vvv
+
+	if [regexp -nocase {([^ ]+) ((is|si|==?) (teh|the)? )?(sucks?)} $text matches 1 2 3 4 5] {
+		set item $1
+		if {$item == "=="} {
+			return 0
+		}
+
+		if {[string tolower $item] == "i"} {
+			return 0
+		}
+
+		if [regexp -nocase "you|we|really|=|i|$botnicks" $item] {
+			return 0
+		}
+
+		if {($2 == "") && ($3 == "suck")} {
+		# no teh or the
+		}
+
+
+
+		bMotionDoAction $channel $item "%VAR{sucks}"
+		return 1
+	}
 }
 
 
