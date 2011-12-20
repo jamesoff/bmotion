5aa21ee20aa3d5434707ff9312559c6f220ced3b
James Seward <james@jamesoff.net>
2011-12-20 23:59:06 +0000

    add a bunch of debug

--- plugins/en/output_colloquial.tcl
+++ plugins/en/output_colloquial.tcl
@@ -24,36 +24,57 @@ proc bMotion_plugin_output_colloq { channel line } {
 
   if [bMotion_plugin_output_colloq_chance $colloq_rate] {
     regsub -all -nocase "\mshould( have|\'ve| of)\M" $line [bMotion_abstract_get "colloq_shouldhave"] line
+		if {$line == ""} {
+			putlog "colloq plugin killed output: 1"
+		}
   }
 
   if [bMotion_plugin_output_colloq_chance $colloq_rate] {
     regsub -all -nocase "\mshouldn't( have|\'ve| of)\M" $line [bMotion_abstract_get "colloq_shouldhavenot"] line
+		if {$line == ""} {
+			putlog "colloq plugin killed output: 2"
+		}
   }
 
   if [bMotion_plugin_output_colloq_chance $colloq_rate] {
     regsub -all -nocase "sort of" $line "sorta" line
     regsub -all -nocase "something" $line "sommat" line
+		if {$line == ""} {
+			putlog "colloq plugin killed output: 3"
+		}
   }
 
   if [bMotion_plugin_output_colloq_chance $colloq_rate] {
     regsub -all -nocase "cheap" $line "cheep" line
     regsub -all -nocase "seam" $line "seem" line
     regsub -all -nocase "mean" $line "meen" line
+		if {$line == ""} {
+			putlog "colloq plugin killed output: 4"
+		}
   }
 
   if [bMotion_plugin_output_colloq_chance $colloq_rate] {
     regsub -all -nocase "exactly" $line "exactily" line
     regsub -all -nocase "separate" $line "seperate" line
     regsub -all -nocase "definitely" $line "definately" line
+		if {$line == ""} {
+			putlog "colloq plugin killed output: 5"
+		}
   }
 
   if [bMotion_plugin_output_colloq_chance $colloq_rate] {
     regsub -all -nocase {\myou\M} $line [bMotion_abstract_get "colloq_you"] line
     regsub -all -nocase {\myour\M} $line [bMotion_abstract_get "colloq_your"] line
+		if {$line == ""} {
+			putlog "colloq plugin killed output: 6"
+		}
   }
 
   if [bMotion_plugin_output_colloq_chance $colloq_rate] {
     regsub -all -nocase "n't" $line "nt" line
+		if {$line == ""} {
+			putlog "colloq plugin killed output: 7"
+		}
   }
 
   if [bMotion_plugin_output_colloq_chance $colloq_rate] {
@@ -73,11 +94,19 @@ proc bMotion_plugin_output_colloq { channel line } {
     append newLine "$word "
   }
   set line $newLine
+		if {$line == ""} {
+			putlog "colloq plugin killed output: 8"
+		}
 
   #don't waste time updating if the line didn't change
   if {$line == $oldLine} {
     return $oldLine
   }
+
+	if {$line == ""} {
+		putlog "colloq plugin recovering output"
+		return $oldLine
+	}
 
   return [string trim [bMotionDoInterpolation $line "" ""]]
 }
