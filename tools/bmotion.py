c9f0b22a2a7a5279932f10fd79f54645649bd6fb
James Seward <james-github@jamesoff.net>
2014-02-02 14:46:51 +0000

    Initial bmotion.py working

--- tools/bmotion.py
+++ tools/bmotion.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import os
+import os.path
 import sys
 import subprocess
 import argparse
@@ -22,15 +23,16 @@ def main():
                             required=True)
     parser_edit.add_argument("--editor",
                             help="which editor to launch; default is EDITOR from env")
-    parser_edit.set_defaults(func=update)
+    parser_edit.set_defaults(func=edit)
 
     args = parser.parse_args()
+    args.path = os.path.expanduser(args.path)
     args.func(args)
 
 def update(args):
     print "--> Attempting to update bMotion from github"
     try:
-        subprocess.call(['/usr/bin/git', 'pull'], cwd=args.path)
+        subprocess.call(['/usr/bin/git', 'pull'], cwd=os.path.join(args.path, 'scripts', 'bmotion'))
     except Exception, e:
         print "    Oh no :( it all broke"
         print e
@@ -38,18 +40,27 @@ def update(args):
 def edit(args):
     if args.file == "eggdrop":
         print "--> Editing eggdrop config"
-        if args.editor == None:
-            editor = os.getenv("EDITOR")
-            if editor == None:
-                print "    Cannot edit without EDITOR being set in your environment, or --editor on my command line"
-                sys.exit(1)
-        else:
-            editor = args.editor
+        filename = "eggdrop.conf"
+    elif args.file == "bmotion":
+        print "--> Editing bmotion config"
+        filename = "scripts/bmotion/local/settings.tcl"
+    else:
+        print "Invalid file to edit specified!"
 
-        try:
-            subprocess.call([editor, os.path.join(args.path, 'eggdrop.conf')])
-        except Exception, e:
-            print "    Unable to launch editor, sorry :("
-            print e
+    if args.editor == None:
+        editor = os.getenv("EDITOR")
+        if editor == None:
+            print "    Cannot edit without EDITOR being set in your environment, or --editor on my command line"
+            sys.exit(1)
+    else:
+        editor = args.editor
+
+    try:
+        subprocess.call([editor, os.path.join(args.path, filename)])
+    except Exception, e:
+        print "    Unable to launch editor, sorry :("
+        print e
 
 
+if __name__ == "__main__":
+    main()
