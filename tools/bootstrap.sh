b116e0c235de233c23f0f418231c86307d07f876
James Seward <james-github@jamesoff.net>
2014-02-02 15:17:54 +0000

    Create bootstrap script

---
+++ tools/bootstrap.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# This script bootstraps a plain machine into having eggdrop and bmotion installed
+
+if [ -f /usr/bin/apt-get ]; then
+	echo -e '\E[34mBMOTION: Updating system and installing packages...'
+	tput sgr0
+	sudo apt-get update
+	sudo apt-get upgrade
+	sudo apt-get install git python tcl8.5-dev gcc redis-server
+fi
+
+if [ -d ~/eggdrop ]; then
+	echo ~/eggdrop already exists
+	echo Please move it out of the way manually first!
+	exit
+fi
+
+echo -e '\E[34mBMOTION: Preparing eggdrop directory'
+tput sgr0
+
+mkdir -p ~/eggdrop/src && cd ~/eggdrop/src || exit 2
+
+echo -e '\E[34mBMOTION: Fetching eggdrop...'
+tput sgr0
+
+[ -f eggdrop1.6.21.tar.bz2 ] || wget ftp://ftp.eggheads.org/pub/eggdrop/source/1.6/eggdrop1.6.21.tar.bz2 || exit 2
+
+echo -e '\E[34mBMOTION: Unpacking and configuring eggdrop...'
+tput sgr0
+
+tar xf eggdrop1.6.21.tar.bz2 || exit 2
+cd eggdrop1.6.21 || exit 2
+./configure || exit 2
+make config || exit 2
+
+echo -e '\E[34mBMOTION: Building eggdrop...'
+tput sgr0
+
+make || exit 2
+
+echo -e '\E[34mBMOTION: Installing eggdrop...'
+tput sgr0
+make install || exit 2
+
+echo -e '\E[34mBMOTION: Installing bmotion...'
+tput sgr0
+cd ../../scripts || exit 2
+git clone https://github.com/jamesoff/bmotion.git
+sudo ln -s ~/eggdrop/scripts/bmotion/tools/bmotion.py /usr/local/bin/bmotion
+
+echo
+echo Finished. Use the /usr/local/bin/bmotion script to configure.
