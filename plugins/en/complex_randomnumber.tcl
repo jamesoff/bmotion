8182bbfbf101220100e2b2d9e741f593866bd5ca
James Seward <james-github@jamesoff.net>
2014-03-05 21:10:28 +0000

    Rewrite regexp for 'tell me a number'
    Fixes #16

--- plugins/en/complex_randomnumber.tcl
+++ plugins/en/complex_randomnumber.tcl
@@ -12,20 +12,18 @@
 # in the modules directory.
 ###############################################################################
 
-bMotion_plugin_add_complex "randomnumber" "%botnicks ((tell|give) me)|(think (of|up)) a (random )?(negative )?number" 100 bMotion_plugin_complex_rndnum "en"
+bMotion_plugin_add_complex "randomnumber" "%botnicks,?:? (tell|give) me a (random )?(negative )?number" 100 bMotion_plugin_complex_rndnum "en"
+bMotion_plugin_add_complex "randomnumber2" "%botnicks,?:? (make|think) (of|up) a (random )?(negative )?number" 100 bMotion_plugin_complex_rndnum "en"
 
 proc bMotion_plugin_complex_rndnum {nick host handle channel text} {
-	global botnicks
-
-	if [regexp -nocase "$botnicks ((tell|give) me)|(think (of|up)) a (random )?(negative )?number(.+)" $text matches bn a b c d rnd neg cond] {
-		if {$cond == ""} {
-			if {$neg != ""} {
-				bMotionDoAction $channel $nick "%%: -%NUMBER{10000}"
-			} else {
-				bMotionDoAction $channel $nick "%%: %NUMBER{10000}"
-			}
-			return 1
+	#TODO: support conditions like "between 10 and 100" or "lower than 10"
+	if [regexp -nocase "a (random )?(negative )?number" $text matches rnd neg] {
+		if {$neg != ""} {
+			bMotionDoAction $channel $nick "%%: -%NUMBER{10000}"
+		} else {
+			bMotionDoAction $channel $nick "%%: %NUMBER{10000}"
 		}
+		return 1
 	}
 }
 
