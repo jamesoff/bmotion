c1dd6aa4de472f4ab19ddbf5843505c03bebe187
James Seward <james-github@jamesoff.net>
2014-04-06 23:41:49 +0100

    Fix indentation

--- plugins/admin_general.tcl
+++ plugins/admin_general.tcl
@@ -28,40 +28,40 @@ bMotion_plugin_add_management "debug" "^debug" n bMotion_plugin_management_debug
 
 proc bMotion_plugin_management_status { handle { args "" } } {
 	global bMotionInfo botnicks bMotionSettings bMotionVersion bMotionChannels BMOTION_SLEEP botnick
-		bMotion_update_chanlist 
+	bMotion_update_chanlist 
 
-		set my_botnick [string toupper $botnick]
+	set my_botnick [string toupper $botnick]
 
-		bMotion_putadmin "I AM $my_botnick! I'm powered by bMotion $bMotionVersion. I'm $bMotionInfo(gender) and this week I have mostly been $bMotionInfo(orientation)."
+	bMotion_putadmin "I AM $my_botnick! I'm powered by bMotion $bMotionVersion. I'm $bMotionInfo(gender) and this week I have mostly been $bMotionInfo(orientation)."
 
-		if {$botnicks == ""} {
-			bMotion_check_botnicks
+	if {$botnicks == ""} {
+		bMotion_check_botnicks
+	}
+
+	bMotion_putadmin "My botnicks are currently /$botnicks/."
+
+	if {$args == "channels"} {
+		bMotion_putadmin "I'm active on: $bMotionChannels"
+	}
+
+	if {[bMotion_setting_get "sleepy"]} {
+		switch $bMotionSettings(asleep) {
+			0 {
+				bMotion_putadmin "I'm currently awake and my next bedtime is [clock format $bMotionSettings(sleepy_nextchange)]."
+			}
+			1 {
+				bMotion_putadmin "I'm currently in my pajamas."
+			}
+			2 {
+				bMotion_putadmin "I'm actually asleep right now. My alarm's set for [clock format $bMotionSettings(sleepy_nextchange)]."
+			}
+			default {
+				bMotion_putadmin "I'm not really sure if I'm awake or what. My current sleep state is $bMotionSettings(asleep) and my next event is [clock format $bMotionSettings(sleepy_nextchange)], although who knows what'll happen then."
+			}
 		}
-
-		bMotion_putadmin "My botnicks are currently /$botnicks/."
-
-		if {$args == "channels"} {
-			bMotion_putadmin "I'm active on: $bMotionChannels"
-		}
-
-		if {[bMotion_setting_get "sleepy"]} {
-				switch $bMotionSettings(asleep) {
-						0 {
-								bMotion_putadmin "I'm currently awake and my next bedtime is [clock format $bMotionSettings(sleepy_nextchange)]."
-						}
-						1 {
-								bMotion_putadmin "I'm currently in my pajamas."
-						}
-						2 {
-								bMotion_putadmin "I'm actually asleep right now. My alarm's set for [clock format $bMotionSettings(sleepy_nextchange)]."
-						}
-						default {
-								bMotion_putadmin "I'm not really sure if I'm awake or what. My current sleep state is $bMotionSettings(asleep) and my next event is [clock format $bMotionSettings(sleepy_nextchange)], although who knows what'll happen then."
-						}
-				}
-		} else {
-				bMotion_putadmin "I am impervious to tiredness and never need to sleep."
-		}
+	} else {
+		bMotion_putadmin "I am impervious to tiredness and never need to sleep."
+	}
 
 	bMotion_putadmin "Random stuff happens at least every [bMotion_setting_get minRandomDelay]min, at most every [bMotion_setting_get maxRandomDelay]min, and not if channel quiet for more than [bMotion_setting_get maxIdleGap]min. Active channels have a line in the last [bMotion_setting_get active_idle_sec]sec."
 	if [bMotion_setting_get silence] {
@@ -70,7 +70,7 @@ proc bMotion_plugin_management_status { handle { args "" } } {
 	if [bMotion_setting_get "away"] {
 		bMotion_putadmin "I am away."
 	}
-	
+
 	global bMotion_loaded_settings_from
 	bMotion_putadmin "I loaded my configuration from: $bMotion_loaded_settings_from"
 
@@ -81,13 +81,13 @@ proc bMotion_plugin_management_queue { handle { args "" }} {
 	global bMotion_queue
 
 	if {$args == ""} {
-		#display queue
+	#display queue
 		bMotion_putadmin "Queue size: [bMotion_queue_size] lines"
-			foreach item $bMotion_queue {
-				set sec [lindex $item 0]
-				set target [lindex $item 1]
-				set content [lindex $item 2]
-				bMotion_putadmin "Delay $sec sec, target $target: $content"
+		foreach item $bMotion_queue {
+			set sec [lindex $item 0]
+			set target [lindex $item 1]
+			set content [lindex $item 2]
+			bMotion_putadmin "Delay $sec sec, target $target: $content"
 		}
 		return 0
 	}
@@ -98,59 +98,59 @@ proc bMotion_plugin_management_queue { handle { args "" }} {
 		return 0
 	}
 
-		if {$args == "freeze"} {
-				bMotion_putadmin "Freezing queue..."
-				bMotion_queue_freeze
-				return 0
-		}
+	if {$args == "freeze"} {
+		bMotion_putadmin "Freezing queue..."
+		bMotion_queue_freeze
+		return 0
+	}
 
-		if {$args == "thaw"} {
-				bMotion_putadmin "Thawing queue..."
-				bMotion_queue_thaw
-				return 0
-		}
+	if {$args == "thaw"} {
+		bMotion_putadmin "Thawing queue..."
+		bMotion_queue_thaw
+		return 0
+	}
 
-		if {$args == "run"} {
-				bMotion_putadmin "Running queue..."
-				bMotion_queue_run 1
-				return 0
-		}
+	if {$args == "run"} {
+		bMotion_putadmin "Running queue..."
+		bMotion_queue_run 1
+		return 0
+	}
 }
 
 proc bMotion_plugin_management_parse { handle { arg "" } } {
-		if {$arg == ""} {
-				bMotion_putadmin "You must supply something to parse"
-				return 0
-		}
-
-		set output [bMotion_plugins_settings_get "admin" "type" "" ""]
-		set target [bMotion_plugins_settings_get "admin" "target" "" ""]
-
-		if {$output == ""} {
-				return 0
-		}
-
-		if {($output == "dcc") || (![string match "#*" $target])} {
-				#command syntax should be:
-				# .bmotion parse #channel output
-				if [regexp -nocase {(#[^ ]+) (.+)} $arg matches chan parse] {
-						bMotionDoAction $chan "somenick" "\[parse\] $parse"
-						bMotion_putadmin "Sent '$parse' to $chan"
-						return 0
-				}
-		}
-
-		#puthelp $target
-		if [regexp {^-n ?([0-9]+) (.+)} $arg matches repeat parse] {
-			putlog "Parsing '$parse' * $repeat in $target"
-			for { set i 0 } { $i < $repeat } { incr i } {
-				bMotionDoAction $target "somenick" $parse
-			}
-		} else {
-			putlog "Parsing '$arg', requested in $target"
-			bMotionDoAction $target "somenick" $arg
-		}
+	if {$arg == ""} {
+		bMotion_putadmin "You must supply something to parse"
 		return 0
+	}
+
+	set output [bMotion_plugins_settings_get "admin" "type" "" ""]
+	set target [bMotion_plugins_settings_get "admin" "target" "" ""]
+
+	if {$output == ""} {
+		return 0
+	}
+
+	if {($output == "dcc") || (![string match "#*" $target])} {
+	#command syntax should be:
+	# .bmotion parse #channel output
+	if [regexp -nocase {(#[^ ]+) (.+)} $arg matches chan parse] {
+		bMotionDoAction $chan "somenick" "\[parse\] $parse"
+		bMotion_putadmin "Sent '$parse' to $chan"
+		return 0
+	}
+	}
+
+	#puthelp $target
+	if [regexp {^-n ?([0-9]+) (.+)} $arg matches repeat parse] {
+	putlog "Parsing '$parse' * $repeat in $target"
+		for { set i 0 } { $i < $repeat } { incr i } {
+	bMotionDoAction $target "somenick" $parse
+	}
+} else {
+	putlog "Parsing '$arg', requested in $target"
+	bMotionDoAction $target "somenick" $arg
+}
+return 0
 }
 
 proc bMotion_plugin_management_rehash { handle } {
@@ -202,8 +202,8 @@ proc bMotion_plugin_management_settings { handle { arg "" } } {
 
 	if {$arg == "clear"} {
 		if {![info exists bMotion_plugins_settings]} {
-				unset bMotion_plugins_settings
-				set bMotion_plugins_settings(dummy,setting,channel,nick) "dummy"
+			unset bMotion_plugins_settings
+			set bMotion_plugins_settings(dummy,setting,channel,nick) "dummy"
 		}
 		bMotion_putadmin "Cleared plugins settings array"
 		return 0
@@ -212,7 +212,7 @@ proc bMotion_plugin_management_settings { handle { arg "" } } {
 	if {$arg == "list"} {
 		set s [array startsearch bMotion_plugins_settings]
 		while {[set key [array nextelement bMotion_plugins_settings $s]] != ""} {
-				bMotion_putadmin "$key = $bMotion_plugins_settings($key)"
+			bMotion_putadmin "$key = $bMotion_plugins_settings($key)"
 		}
 		array donesearch bMotion_plugins_settings $s
 	}
@@ -248,79 +248,79 @@ proc bMotion_plugin_management_global { handle { text "" } } {
 }
 
 proc bMotion_plugin_management_interbot { handle { text "" } } {
-		global bMotion_interbot_nextbot_nick
-		
-		if [regexp -nocase "next (#.+)" $text matches chan] {
-				set next $bMotion_interbot_nextbot_nick($chan)
-				if {$next == ""} {
-						bMotion_putadmin "Next bot is unknown!"
-				} else {
-						bMotion_putadmin "Next bot for $chan is $next"
-				}
+	global bMotion_interbot_nextbot_nick
+
+	if [regexp -nocase "next (#.+)" $text matches chan] {
+		set next $bMotion_interbot_nextbot_nick($chan)
+		if {$next == ""} {
+			bMotion_putadmin "Next bot is unknown!"
+		} else {
+			bMotion_putadmin "Next bot for $chan is $next"
 		}
+	}
 
-		if [regexp -nocase "elect (#.+)" $text matches chan] {
-				bMotion_putadmin "Forcing election on $chan..."
+	if [regexp -nocase "elect (#.+)" $text matches chan] {
+		bMotion_putadmin "Forcing election on $chan..."
 
-				bMotion_interbot_next_elect_do $chan
+		bMotion_interbot_next_elect_do $chan
+	}
+
+	if [regexp -nocase "bots (#.+)" $text matches chan] {
+
+		bMotion_putadmin "Known bMotion bots on $chan: "
+		bMotion_putadmin [bMotion_interbot_otherbots $chan]
+	}
+
+	if [regexp -nocase "enable" $text] {
+		global bMotion_interbot_enable
+
+		if {$bMotion_interbot_enable} {
+			bMotion_putadmin "Interbot stuff is already enabled."
+			return
+		} else {
+			set bMotion_interbot_enable 1
+			bMotion_putadmin "Interbot stuff enabled."
+			return
 		}
+	}
 
-		if [regexp -nocase "bots (#.+)" $text matches chan] {
+	if [regexp -nocase "disable" $text] {
+		global bMotion_interbot_enable
 
-				bMotion_putadmin "Known bMotion bots on $chan: "
-				bMotion_putadmin [bMotion_interbot_otherbots $chan]
+		if {!$bMotion_interbot_enable} {
+			bMotion_putadmin "Interbot stuff is already disabled."
+			return
+		} else {
+			set bMotion_interbot_enable 0
+			bMotion_putadmin "Interbot stuff disabled."
+			return
 		}
-
-		if [regexp -nocase "enable" $text] {
-				global bMotion_interbot_enable
-
-				if {$bMotion_interbot_enable} {
-						bMotion_putadmin "Interbot stuff is already enabled."
-						return
-				} else {
-						set bMotion_interbot_enable 1
-						bMotion_putadmin "Interbot stuff enabled."
-						return
-				}
-		}
-
-		if [regexp -nocase "disable" $text] {
-				global bMotion_interbot_enable
-
-				if {!$bMotion_interbot_enable} {
-						bMotion_putadmin "Interbot stuff is already disabled."
-						return
-				} else {
-						set bMotion_interbot_enable 0
-						bMotion_putadmin "Interbot stuff disabled."
-						return
-				}
-		}
+	}
 }
 
 proc bMotion_plugin_management_interbot_help { } {
-		bMotion_putadmin "Manage the interbot communication stuff."
-		bMotion_putadmin "	.bmotion interbot next #channel"
-		bMotion_putadmin "		Report the next bot for #channel"
-		bMotion_putadmin "	.bmotion interbot elect #channel"
-		bMotion_putadmin "		Force an election for #channel"
-		bMotion_putadmin "	.bmotion interbot bots #channel"
-		bMotion_putadmin "		Report bots discovered in #channel"
-		bMotion_putadmin "	.bmotion interbot enable"
-		bMotion_putadmin "	.bmotion interbot disable"
-		bMotion_putadmin "		Enable/disable interbot stuff entirely"
+	bMotion_putadmin "Manage the interbot communication stuff."
+	bMotion_putadmin "	.bmotion interbot next #channel"
+	bMotion_putadmin "		Report the next bot for #channel"
+	bMotion_putadmin "	.bmotion interbot elect #channel"
+	bMotion_putadmin "		Force an election for #channel"
+	bMotion_putadmin "	.bmotion interbot bots #channel"
+	bMotion_putadmin "		Report bots discovered in #channel"
+	bMotion_putadmin "	.bmotion interbot enable"
+	bMotion_putadmin "	.bmotion interbot disable"
+	bMotion_putadmin "		Enable/disable interbot stuff entirely"
 }
 
 # um
 proc bMotion_plugin_management_flux { handle { text "" } } {
-		if {[string match "*off*" $text]} {
-				bMotion_putadmin "i call it... MISTAR FUSION!"
-				return
-		}
-		if {[string match "*on*" $text]} {
-				bMotion_putadmin "ONE POINT TWENTY ONE JIGAWATS"
-				return
-		}
+	if {[string match "*off*" $text]} {
+		bMotion_putadmin "i call it... MISTAR FUSION!"
+		return
+	}
+	if {[string match "*on*" $text]} {
+		bMotion_putadmin "ONE POINT TWENTY ONE JIGAWATS"
+		return
+	}
 }
 
 proc bMotion_plugin_management_away { handle { text "" } } {
@@ -331,7 +331,7 @@ proc bMotion_plugin_management_away { handle { text "" } } {
 			bMotion_putadmin "I am currently away."
 		} else {
 			bMotion_putadmin "I am not away."
-			}
+		}
 		return
 	}
 
