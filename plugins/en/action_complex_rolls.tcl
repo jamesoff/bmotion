ba06f3c896cc421eceead04a38a7a34a2b61d08a
James Seward <james@jamesoff.net>
2014-08-05 22:45:42 +0100

    New plugin: belly rubs

--- plugins/en/action_complex_rolls.tcl
+++ plugins/en/action_complex_rolls.tcl
@@ -12,6 +12,7 @@
 ###############################################################################
 
 bMotion_plugin_add_action_complex "rolls" "rolls %botnicks" 100 bMotion_plugin_complex_action_rolls "en"
+bMotion_plugin_add_action_complex "bellyrubs" "rolls over for .*rubs" 80 bMotion_plugin_complex_action_bellyrubs "en"
 
 proc bMotion_plugin_complex_action_rolls { nick host handle channel text } {
 	bMotion_putloglev d * "bMotion: Got rolled"
@@ -19,9 +20,24 @@ proc bMotion_plugin_complex_action_rolls { nick host handle channel text } {
 	return 1
 }
 
+
+proc bMotion_plugin_complex_action_bellyrubs { nick host handle channel text } {
+	if {[bMotion_interbot_me_next $channel]} {
+		bMotionDoAction $channel $nick "%VAR{belly_rubs}"
+		return 1
+	}
+}
+
+
 bMotion_abstract_register "rolls_channel_response" {
 	"keep them doggies rolling RAWHIDE"
 	"rolls a joint"
 	". o O ( ? )"
 	"%VAR{fellOffs}"
+}
+
+bMotion_abstract_register "belly_rubs" {
+	"/tickles %%"
+	"/springs that trap"
+	"/rubs %%%|%!who's a good boy and/or girl? %SMILEY{bigsmile}"
 }
