8c31c70ab3d509c0cce526d950e5cc8f1ab6ecd8
James Seward <james@jamesoff.net>
2014-11-15 13:17:15 +0000

    Retab leet.tcl

--- modules/leet.tcl
+++ modules/leet.tcl
@@ -23,114 +23,117 @@
 #load alphaset transforms
 source "$bMotionModules/leet_settings.tcl"
 
+
 # Binds
 bind pub - "!leet" bMotionLeetChannel
 bind msg - leet leetPrivate
 
+
 proc bMotionLeetChannel {nick host handle channel text} {
-  global botnick bMotionInfo
-  if {$bMotionInfo(balefire) != 1} {
-    return 0
-  }
-  puthelp "PRIVMSG [chandname2name $channel] :[makeLeet2 $text]"
+	global botnick bMotionInfo
+	if {$bMotionInfo(balefire) != 1} {
+		return 0
+	}
+	puthelp "PRIVMSG [chandname2name $channel] :[makeLeet2 $text]"
 }
+
 
 proc makeLeet { line } {
-  return "Warning! Call to old makeLeet."
+	return "Warning! Call to old makeLeet."
 }
+
 
 proc makeLeet2 {line} {
-  global leet bMotionInfo
-  if [rand 2] { regsub -nocase -all {(\d+)} $line "1337" line }
-  #if [rand 2] { regsub -nocase -all "hackers" $line "hax0rz" line }
-  if [rand 2] { regsub -nocase -all "cool" $line "kewl" line }
-  if [rand 2] { regsub -nocase -all "dudes" $line "d00dz" line }
-  if [rand 2] { regsub -nocase -all "dude" $line "d00d" line }
-  #if [rand 2] { regsub -nocase -all "er" $line "hax0r" line }
-  #if [rand 2] { regsub -nocase -all "ed" $line "hax0red" line }
-  #if [rand 2] { regsub -nocase -all "owned" $line "own0red" line }
-  #if [rand 2] { regsub -nocase -all "rocks" $line "rox0rs" line }
-  #if [rand 2] { regsub -nocase -all "rock" $line "r0x0r" line }
-  if [rand 2] { regsub -nocase -all "boxes" $line "b0xen" line }
-  if [rand 2] { regsub -nocase -all "porn" $line "pr0n" line }
-  if [rand 2] { regsub -nocase -all "elite" $line "l33t" line }
-  if [rand 2] { regsub -nocase -all "your" $line "j00r" line }
-  if [rand 2] { regsub -nocase -all "fear" $line "ph33r" line }
-  if [rand 2] { regsub -nocase -all "wins" $line "win0rs" line }
-  if [rand 2] { regsub -nocase -all "you" $line "j00" line }
-  if [rand 2] { regsub -nocase -all "money" $line "monies" line }
-  if [rand 2] { regsub -nocase -all "like" $line "liek" line }
-  if [rand 2] { regsub -nocase -all "hacking" $line "hax0ring" line }
-  if [rand 2] { regsub -nocase -all "skills" $line "skillz" line }
+	global leet bMotionInfo
+	if [rand 2] { regsub -nocase -all {(\d+)} $line "1337" line }
+	#if [rand 2] { regsub -nocase -all "hackers" $line "hax0rz" line }
+	if [rand 2] { regsub -nocase -all "cool" $line "kewl" line }
+	if [rand 2] { regsub -nocase -all "dudes" $line "d00dz" line }
+	if [rand 2] { regsub -nocase -all "dude" $line "d00d" line }
+	#if [rand 2] { regsub -nocase -all "er" $line "hax0r" line }
+	#if [rand 2] { regsub -nocase -all "ed" $line "hax0red" line }
+	#if [rand 2] { regsub -nocase -all "owned" $line "own0red" line }
+	#if [rand 2] { regsub -nocase -all "rocks" $line "rox0rs" line }
+	#if [rand 2] { regsub -nocase -all "rock" $line "r0x0r" line }
+	if [rand 2] { regsub -nocase -all "boxes" $line "b0xen" line }
+	if [rand 2] { regsub -nocase -all "porn" $line "pr0n" line }
+	if [rand 2] { regsub -nocase -all "elite" $line "l33t" line }
+	if [rand 2] { regsub -nocase -all "your" $line "j00r" line }
+	if [rand 2] { regsub -nocase -all "fear" $line "ph33r" line }
+	if [rand 2] { regsub -nocase -all "wins" $line "win0rs" line }
+	if [rand 2] { regsub -nocase -all "you" $line "j00" line }
+	if [rand 2] { regsub -nocase -all "money" $line "monies" line }
+	if [rand 2] { regsub -nocase -all "like" $line "liek" line }
+	if [rand 2] { regsub -nocase -all "hacking" $line "hax0ring" line }
+	if [rand 2] { regsub -nocase -all "skills" $line "skillz" line }
 
-  #...cked -> x0r3d
-  if [rand 2] { regsub -nocase -all {\m(\w+?)?cked\M} $line {\1x0r3d} line }
+	#...cked -> x0r3d
+	if [rand 2] { regsub -nocase -all {\m(\w+?)?cked\M} $line {\1x0r3d} line }
 
-  #...cker(s) -> x0r(s)
-  if [rand 2] { regsub -nocase -all {\m(\w+?)?cker(s|z)?\M} $line {\1x0r\2} line }
+	#...cker(s) -> x0r(s)
+	if [rand 2] { regsub -nocase -all {\m(\w+?)?cker(s|z)?\M} $line {\1x0r\2} line }
 
-  #[constonant]ed -> 0r3d
-  if [rand 2] { regsub -nocase -all {***:(\w+[^aeiouy])ed\M} $line {\10r3d} line }
+	#[constonant]ed -> 0r3d
+	if [rand 2] { regsub -nocase -all {***:(\w+[^aeiouy])ed\M} $line {\10r3d} line }
 
-  #...s --> ...z
-  if [rand 2] { regsub -nocase -all {(\w+)s\M} $line {\1z} line }
+	#...s --> ...z
+	if [rand 2] { regsub -nocase -all {(\w+)s\M} $line {\1z} line }
 
-  #f... -> oph...
-  if [rand 2] { regsub -nocase -all {\mf+(\w+)} $line {ph\1} line }
+	#f... -> oph...
+   if [rand 2] { regsub -nocase -all {\mf+(\w+)} $line {ph\1} line }
 
-  #...f -> ...oph
-  if [rand 2] { regsub -nocase -all {(\w+?)f+\M} $line {\1ph} line }
-  if [rand 2] { set line [string map -nocase { ROFL roflmaolozz!!111 loser lossarzz!! hehe HUHEHUHEHEHUEHUEH ike iek ter tar ife ief hah hehue ule lue ota oat ver var is si ome oem ame aem oe eo aid iad ers ars erz arz per par nic nix aye aey ade aed ite eit} $line ] }
-  if [rand 2] { set line [string map -nocase { he eh re er ea ae hi ih or ro ip pi ho oh in ni lol lo!lololololzz! ! !!!111111 ir ri ou uo ha ah ui iu ig gi } $line ] }
-  set letters [split [string tolower $line] {}]
-  set line ""
+   #...f -> ...oph
+	if [rand 2] { regsub -nocase -all {(\w+?)f+\M} $line {\1ph} line }
+	if [rand 2] { set line [string map -nocase { ROFL roflmaolozz!!111 loser lossarzz!! hehe HUHEHUHEHEHUEHUEH ike iek ter tar ife ief hah hehue ule lue ota oat ver var is si ome oem ame aem oe eo aid iad ers ars erz arz per par nic nix aye aey ade aed ite eit} $line ] }
+	if [rand 2] { set line [string map -nocase { he eh re er ea ae hi ih or ro ip pi ho oh in ni lol lo!lololololzz! ! !!!111111 ir ri ou uo ha ah ui iu ig gi } $line ] }
+	set letters [split [string tolower $line] {}]
+	set line ""
 
-  set alphabet [split "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" {}]
+	set alphabet [split "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" {}]
 
-  foreach letter $letters {
-    set newChar $letter
-    if {[rand 10] > $bMotionInfo(leetChance)} {
-      if {[regexp -nocase {[A-Za-z]} $letter]} {
-        set newChar [pickRandom $leet($letter)]
-      }
-    }
+	foreach letter $letters {
+		set newChar $letter
+		if {[rand 10] > $bMotionInfo(leetChance)} {
+			if {[regexp -nocase {[A-Za-z]} $letter]} {
+				set newChar [pickRandom $leet($letter)]
+			}
+		}
 
-    if [rand 2] {
-      set newChar [string toupper $newChar]
-    } else {
-      set newChar [string tolower $newChar]
-    }
-    set line "$line$newChar"
-  }
+		if [rand 2] {
+			set newChar [string toupper $newChar]
+		} else {
+			set newChar [string tolower $newChar]
+		}
+		set line "$line$newChar"
+	}
 
-  return $line
+	return $line
 }
 
-#end of makeleet2
 
 proc leetPrivate {nick host handle arg} {
-  #set handle [finduser $host]
-  if {$handle == "*"} {
-    return 0
-  }
-  if [regexp {(\[#!\]\w+) (.+)} $arg ming channel param] {
-    set val [makeLeet2 $param]
-    putlog "bMotion: $nick asked !$param! to be leeted to !$channel!"
-    if {![botonchan $channel]} {
-      puthelp "PRIVMSG $nick :Sorry, I'm not on $channel"
-      return 0
-    }
-    if {![onchan $nick $channel]} {
-      set name [bMotionTransformNick $nick $nick $host]
-      puthelp "PRIVMSG $nick :I'm sorry $name, I can't do that."
-      putlog "bMotion: ALERT! $nick failed query leet to $channel ($param)."
-      return 0
-    }
-    puthelp "PRIVMSG [chandname2name $channel] :\[\002$nick\002\] $val"
-    return 0
-  }
+	if {$handle == "*"} {
+		return 0
+	}
+	if [regexp {(\[#!\]\w+) (.+)} $arg ming channel param] {
+		set val [makeLeet2 $param]
+		putlog "bMotion: $nick asked !$param! to be leeted to !$channel!"
+		if {![botonchan $channel]} {
+			puthelp "PRIVMSG $nick :Sorry, I'm not on $channel"
+			return 0
+		}
+		if {![onchan $nick $channel]} {
+			set name [bMotionTransformNick $nick $nick $host]
+			puthelp "PRIVMSG $nick :I'm sorry $name, I can't do that."
+			putlog "bMotion: ALERT! $nick failed query leet to $channel ($param)."
+			return 0
+		}
+		puthelp "PRIVMSG [chandname2name $channel] :\[\002$nick\002\] $val"
+		return 0
+	}
 
-  putlog "bMotion: $nick asked !$arg! to be leeted"
-  puthelp "PRIVMSG $nick :[makeLeet2 $arg]"
-  return 0
+	putlog "bMotion: $nick asked !$arg! to be leeted"
+	puthelp "PRIVMSG $nick :[makeLeet2 $arg]"
+	return 0
 }
+
