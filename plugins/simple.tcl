77e03fbc0edc02df8627ca2cf725b7c8e342b792
James Seward <james@jamesoff.net>
2015-06-08 10:41:50 -0400

    Change simple.tcl to newlogging

--- plugins/simple.tcl
+++ plugins/simple.tcl
@@ -13,19 +13,19 @@ set currentlang $bMotionInfo(language)
 set languages [split $bMotionSettings(languages) ","]
 foreach bMotion_language $languages {
 	set bMotionInfo(language) $bMotion_language
-	bMotion_putloglev 2 * "bMotion: loading simple plugins language = $bMotion_language"
+	bMotion_log "plugins" "DEBUG" "loading simple plugins language = $bMotion_language"
 	set files [glob -nocomplain "$bMotionPlugins/$bMotion_language/simple_*.tcl"]
 	foreach f $files {
 		set count [llength [array names bMotion_plugins_simple]]
-		bMotion_putloglev 1 * "bMotion: loading ($bMotion_language) simple plugin file $f"
+		bMotion_log "plugins" "DEBUG" "loading ($bMotion_language) simple plugin file $f"
 		set bMotion_noplugins 0
 		catch {
 			source $f
 		} err
 		set newcount [llength [array names bMotion_plugins_simple]]
 		if {($err != "") && ($err != "1") && ($bMotion_testing == 0) && ($newcount == $count) && ($bMotion_noplugins == 0)} {
-			putlog "bMotion: ALERT! Loading plugins file $f did not add any plugins!"
-			putlog "Possible error: $err"
+			bMotion_log "plugins" "ERROR" "ALERT! Loading plugins file $f did not add any plugins!"
+			bMotion_log "plugins" "ERROR" "Possible error: $err"
 		}
 	}
 }
