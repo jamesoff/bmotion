4f15cd146bde8b6d77522da1517885ab3376487e
James Seward <james@jamesoff.net>
2016-01-22 22:43:41 +0000

    .

--- tests/build-eggdrop.sh
+++ tests/build-eggdrop.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-wget -LO http://www.eggheads.org/redirect.php?url=ftp://ftp.eggheads.org/pub/eggdrop/source/1.6/eggdrop1.6.21.tar.gz
+wget -L http://www.eggheads.org/redirect.php?url=ftp://ftp.eggheads.org/pub/eggdrop/source/1.6/eggdrop1.6.21.tar.gz
 tar xvf eggdrop1.6.21.tar.gz
 cd eggdrop1.6.21
 ./configure
