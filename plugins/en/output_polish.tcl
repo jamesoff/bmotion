5f872858364191af371e6518e44ddbe8563fe528
James Seward <james@jamesoff.net>
2016-08-26 15:12:24 +0200

    WIP polish output plugin

---
+++ plugins/en/output_polish.tcl
@@ -0,0 +1,47 @@
+#
+#
+# vim: fdm=indent fdn=1
+
+###############################################################################
+# This is a bMotion plugin
+# Copyright (C) James Michael Seward 2000-2008
+#
+# This program is covered by the GPL, please refer the to LICENCE file in the
+# distribution; further information can be found in the headers of the scripts
+# in the modules directory.
+###############################################################################
+
+bMotion_plugin_add_output "polish" bMotion_plugin_output_polish 0 "en" 11
+
+
+proc bMotion_plugin_output_polish { channel line } {
+    set line [string trim $line]
+
+    if {[string range $line 0 0] == "/"} {
+        return $line
+    }
+
+    set newline ""
+    set words [split $line " "]
+
+    foreach word $words {
+        #not for one letter words
+        if {[string length $word] == 1} {
+            append newline "$word "
+            continue
+        }
+
+        # words ending with "s" get "with a z" added
+        # TODO: punctuation
+        if {![regexp -nocase {([sS])$} $word m a]} {
+            if {$a == 's'} {
+                append newline "$word with a z "
+            } else {
+                append newline "$word WITH A Z "
+            }
+            continue
+        }
+        append newline "$word "
+    }
+    return $newline
+}
