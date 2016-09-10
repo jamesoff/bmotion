85ef15d06e4ab0760420c4b826334b703c5a02a7
James Seward <james@jamesoff.net>
2016-09-10 19:05:43 +0100

    Retab output.tcl

--- plugins/output.tcl
+++ plugins/output.tcl
@@ -14,21 +14,21 @@
 
 set languages [split $bMotionSettings(languages) ","]
 foreach bMotion_language $languages {
-  bMotion_putloglev 2 * "bMotion: loading output plugins language = $bMotion_language"
-  set files [glob -nocomplain "$bMotionPlugins/$bMotion_language/output_*.tcl"]
-  foreach f $files {
+	bMotion_putloglev 2 * "bMotion: loading output plugins language = $bMotion_language"
+	set files [glob -nocomplain "$bMotionPlugins/$bMotion_language/output_*.tcl"]
+	foreach f $files {
 		set count [llength [array names bMotion_plugins_output]]
-    bMotion_putloglev 1 * "bMotion: loading ($bMotion_language) output plugin file $f"
+		bMotion_putloglev 1 * "bMotion: loading ($bMotion_language) output plugin file $f"
 		set bMotion_noplugins 0
-    catch {
-      source $f
-    } err
+		catch {
+			source $f
+		} err
 		set newcount [llength [array names bMotion_plugins_output]]
 		if {($bMotion_testing == 0) && ($newcount == $count) && ($bMotion_noplugins == 0)} {
 			putlog "bMotion: ALERT! output plugin file $f added no plugins"
 			putlog "Possible error: $err"
 		}
-  }
+	}
 }
 
 # enable or disable plugins as per the settings file
