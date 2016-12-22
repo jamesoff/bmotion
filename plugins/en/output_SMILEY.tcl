63daa04a202e2e7a7ea73571dcc78e09a49eac0b
James Seward <james@jamesoff.net>
2016-12-22 14:17:20 +0000

    Reformat output_SMILEY

--- plugins/en/output_SMILEY.tcl
+++ plugins/en/output_SMILEY.tcl
@@ -22,27 +22,27 @@
 proc bMotion_plugin_output_SMILEY { channel line } {
     bMotion_log "output" "TRACE" "bMotion_plugin_output_SMILEY $channel $line"
 
-	set smiley_type [bMotion_setting_get "smileytype"]
-	set smiley_nose_type [bMotion_setting_get "smileynosetype"]
+    set smiley_type [bMotion_setting_get "smileytype"]
+    set smiley_nose_type [bMotion_setting_get "smileynosetype"]
 
-	if {[regexp {(%SMILEY?\{([^\}:]+)(:([^\}])+)?\})} $line matches whole_thing type options]} {
+    if {[regexp {(%SMILEY?\{([^\}:]+)(:([^\}])+)?\})} $line matches whole_thing type options]} {
         bMotion_log "output" "TRACE" "output_smiley: type=$type options=$options"
 
-		set smiley [bMotion_get_smiley $type]
-		if {$smiley == ""} {
+        set smiley [bMotion_get_smiley $type]
+        if {$smiley == ""} {
             bMotion_log "output" "WARN" "Unable to get smiley for type $type"
-		}
+        }
 
-		set location [string first $whole_thing $line]
-		if {$location == -1} {
-			bMotion_log "output" "ERROR" "error parsing $whole_thing in $line, unable to insert $smiley"
-			return ""
-		}
+        set location [string first $whole_thing $line]
+        if {$location == -1} {
+            bMotion_log "output" "ERROR" "error parsing $whole_thing in $line, unable to insert $smiley"
+            return ""
+        }
 
-		set line [string replace $line $location [expr $location + [string length $whole_thing] - 1] $smiley]
-	}
+        set line [string replace $line $location [expr $location + [string length $whole_thing] - 1] $smiley]
+    }
 
-	return $line
+    return $line
 }
 
 bMotion_plugin_add_output "SMILEY" bMotion_plugin_output_SMILEY 1 "en" 5
