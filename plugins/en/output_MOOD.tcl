7b6352c59e348e1d01f13c9a2555eb30ed9877eb
James Seward <james@jamesoff.net>
2016-12-22 14:21:26 +0000

    Switch output_MOOD to newlogging

--- plugins/en/output_MOOD.tcl
+++ plugins/en/output_MOOD.tcl
@@ -16,22 +16,22 @@
 
 
 proc bMotion_plugin_output_MOOD { channel line } {
-	bMotion_putloglev 4 * "bMotion_plugin_output_MOOD $channel $line"
+    bMotion_log "output" "TRACE" "bMotion_plugin_output_MOOD $channel $line"
 
-	if {[regexp {(%MOOD\{([^\}:]+)(:([^\}])+)?\})} $line matches whole_thing type change]} {
-		set change [string range $change 1 end]
-		bMotion_putloglev 1 * "drifting mood $type by $change from output"
-		bMotion_mood_adjust $type $change
+    if {[regexp {(%MOOD\{([^\}:]+)(:([^\}])+)?\})} $line matches whole_thing type change]} {
+        set change [string range $change 1 end]
+        bMotion_log "output" "DEBUG" "drifting mood $type by $change from output"
+        bMotion_mood_adjust $type $change
 
-		set location [string first $whole_thing $line]
-		if {$location == -1} {
-			putlog "bMotion: error parsing $whole_thing in $line, unable to remove MOOD element"
-			return ""
-		}
-		set line [string replace $line $location [expr $location + [string length $whole_thing] - 1] ""]
-	}
+        set location [string first $whole_thing $line]
+        if {$location == -1} {
+            putlog "bMotion: error parsing $whole_thing in $line, unable to remove MOOD element"
+            return ""
+        }
+        set line [string replace $line $location [expr $location + [string length $whole_thing] - 1] ""]
+    }
 
-	return $line
+    return $line
 }
 
 bMotion_plugin_add_output "MOOD" bMotion_plugin_output_MOOD 1 "en" 5
