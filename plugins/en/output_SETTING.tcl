dbdb5778fb4700ed9b30ee00eafb00f5384a5c97
James Seward <james@jamesoff.net>
2016-12-22 14:18:15 +0000

    Switch output_SETTING to newlogging

--- plugins/en/output_SETTING.tcl
+++ plugins/en/output_SETTING.tcl
@@ -14,21 +14,21 @@
 # built-in processing, %SETTING
 
 proc bMotion_plugin_output_SETTING { channel line } {
-	bMotion_putloglev 4 * "bMotion_plugin_output_SETTING $channel $line"
+    bMotion_log "output" "TRACE" "bMotion_plugin_output_SETTING $channel $line"
 
-	if {[regexp "%SETTING\{(.+?)\}" $line matches settingString]} {
-		set var ""
-		if [regexp {([^:]+:[^:]+):([^:]+):([^:]+):([^:]+)} $settingString matches plugin setting ch ni] {
-			set var [bMotion_plugins_settings_get $plugin $setting $ch $ni]
-		}
-		if {$var == ""} {
-			putlog "bMotion: ALERT! couldn't find setting $settingString (dropping output)"
-			return ""
-		}
-		set line [bMotionInsertString $line "%SETTING{$settingString}" $var]
-	}
+    if {[regexp "%SETTING\{(.+?)\}" $line matches settingString]} {
+        set var ""
+        if [regexp {([^:]+:[^:]+):([^:]+):([^:]+):([^:]+)} $settingString matches plugin setting ch ni] {
+            set var [bMotion_plugins_settings_get $plugin $setting $ch $ni]
+        }
+        if {$var == ""} {
+            putlog "bMotion: ALERT! couldn't find setting $settingString (dropping output)"
+            return ""
+        }
+        set line [bMotionInsertString $line "%SETTING{$settingString}" $var]
+    }
 
-	return $line
+    return $line
 }
 
 bMotion_plugin_add_output "SETTING" bMotion_plugin_output_SETTING 1 "en" 5
