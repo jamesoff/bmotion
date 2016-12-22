9e73bbb6b882653a8184b9440ceb24d9df0b4d11
James Seward <james@jamesoff.net>
2016-12-22 13:52:02 +0000

    Reformat output_ruser

--- plugins/en/output_ruser.tcl
+++ plugins/en/output_ruser.tcl
@@ -15,30 +15,30 @@
 proc bMotion_plugin_output_ruser { channel line } {
     bMotion_log "output" "TRACE" "bMotion_plugin_output_ruser $channel $line"
 
-	if [regexp "%ruser(\{(\[a-z\]*)?(:(\[a-z,\]+))?\})?" $line matches allopts filter optstring options] {
+    if [regexp "%ruser(\{(\[a-z\]*)?(:(\[a-z,\]+))?\})?" $line matches allopts filter optstring options] {
         bMotion_log "output" "DEBUG" "found %ruser with filter=$filter and options=$options"
 
-		set ruser [bMotionGetRealName [bMotion_choose_random_user $channel 0 $filter] ""]
+        set ruser [bMotionGetRealName [bMotion_choose_random_user $channel 0 $filter] ""]
 
-		set options_list [split $options ","]
-		foreach option $options_list {
+        set options_list [split $options ","]
+        foreach option $options_list {
             bMotion_log "output" "DEBUG" "working on option $option"
-			switch $option {
-				"owner" {
-					set ruser [bMotionMakePossessive $ruser]
-				}
-				"caps" {
-					set ruser [string toupper $ruser]
-				}
-				default {
-					bMotion_log "output" "ERROR" "unexpected %ruser option $option in $matches"
-				}
-			}
-		}
-		regsub $matches $line $ruser line
-	}
+            switch $option {
+                "owner" {
+                    set ruser [bMotionMakePossessive $ruser]
+                }
+                "caps" {
+                    set ruser [string toupper $ruser]
+                }
+                default {
+                    bMotion_log "output" "ERROR" "unexpected %ruser option $option in $matches"
+                }
+            }
+        }
+        regsub $matches $line $ruser line
+    }
 
-	return $line
+    return $line
 }
 
 bMotion_plugin_add_output "ruser" bMotion_plugin_output_ruser 1 "en" 3
