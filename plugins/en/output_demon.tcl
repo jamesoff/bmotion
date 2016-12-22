e466a8cb4f1c1c936dccd1e8508d87eafb15ea38
James Seward <james@jamesoff.net>
2016-12-22 14:09:22 +0000

    Switch output_demon to newlogging

--- plugins/en/output_demon.tcl
+++ plugins/en/output_demon.tcl
@@ -16,47 +16,47 @@
 # transform things in a fashion similar to people who work at demon internet
 
 proc bMotion_plugin_output_demon { channel line } {
-	bMotion_putloglev 4 * "bMotion_plugin_output_demon $channel $line"
+    bMotion_log "output" "TRACE" "bMotion_plugin_output_demon $channel $line"
 
-	set newLine ""
+    set newLine ""
 
-	#split words
-	set line [string trim $line]
-	set words [split $line " "]
+    #split words
+    set line [string trim $line]
+    set words [split $line " "]
 
-	foreach word $words {
+    foreach word $words {
 
-		regsub -all {c?k\M} $word {q} word
+        regsub -all {c?k\M} $word {q} word
 
-		regsub -all {o([b-df-hj-np-tv-xz])e(s|ing)?\M} $word {oa\1\2} word
+        regsub -all {o([b-df-hj-np-tv-xz])e(s|ing)?\M} $word {oa\1\2} word
 
-		# undo one -> oan (only if entire word)
-		if {$word == "oan"} {
-			set word "one"
-		}
+        # undo one -> oan (only if entire word)
+        if {$word == "oan"} {
+            set word "one"
+        }
 
-		#other special cases to fix
-		set word [string map -nocase { boaxs boxes soam some someoan someone oq ok } $word]
+        #other special cases to fix
+        set word [string map -nocase { boaxs boxes soam some someoan someone oq ok } $word]
 
-		# [@     enid] i can not be represented by mere regexps.
-		set word [string map -nocase { chiq chyq } $word]
+        # [@     enid] i can not be represented by mere regexps.
+        set word [string map -nocase { chiq chyq } $word]
 
-		# doesn't really work right
-		#regsub -all {le(s)?\M} $word {al\1} word
+        # doesn't really work right
+        #regsub -all {le(s)?\M} $word {al\1} word
 
-		# moving on...
-		set word [string map -nocase { ain ane } $word]
-		regsub -all {([a-z][aeiou])ck\M} $word {\1q} word
-		regsub -all "don'?t" $word "doan" word
-		regsub -all "didn'?t" $word "din" word
-		append newLine "$word "
-	}
+        # moving on...
+        set word [string map -nocase { ain ane } $word]
+        regsub -all {([a-z][aeiou])ck\M} $word {\1q} word
+        regsub -all "don'?t" $word "doan" word
+        regsub -all "didn'?t" $word "din" word
+        append newLine "$word "
+    }
 
-	set line [string map { senior seior junior juior genius geious } $line]
+    set line [string map { senior seior junior juior genius geious } $line]
 
-	set line [string trim $newLine]
+    set line [string trim $newLine]
 
-	return $line
+    return $line
 }
 
 bMotion_plugin_add_output "demon" bMotion_plugin_output_demon 0 "en" 11
